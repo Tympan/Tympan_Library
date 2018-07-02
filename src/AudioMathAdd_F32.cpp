d1f56c34ddc7ac85ba69710a0214507bae61672a
Chip Audette <chipaudette@yahoo.com>
2018-07-02 16:31:45 -0400

    Add or Clarify MathAdd, MathOffset, MathScale

---
+++ src/AudioMathAdd_F32.cpp
@@ -0,0 +1,20 @@
+#include "AudioMathAdd_F32.h"
+
+void AudioMathAdd_F32::update(void) {
+  audio_block_f32_t *block, *in;
+
+  block = AudioStream_F32::receiveWritable_f32(0);
+  if (!block) return;
+
+  in = AudioStream_F32::receiveReadOnly_f32(1);
+  if (!in) {
+    release(block);
+    return;
+  }
+
+  arm_add_f32(block->data, in->data, block->data, block->length);
+  release(in);
+
+  transmit(block);
+  release(block);
+}
--- src/AudioMathOffset_F32.h
+++ src/AudioMathOffset_F32.h
@@ -1,16 +1,16 @@
 /*
- * AudioMathOffsets
+ * AudioMathOffset_F32
  * 
  * Created: Chip Audette, Open Audio, June 2018
- * Purpose: Add a fixed DC offset (single value, not vector) to the audio data
+ * Purpose: Add a constant DC value to all audio samples
  * Assumes floating-point data.
  *          
  * This processes a single stream fo audio data (ie, it is mono)       
  *          
  * MIT License.  use at your own risk.
 */
-#ifndef AudioMathAdd_F32_H
-#define AudioMathAdd_F32_H
+#ifndef AudioMathOffset_F32_H
+#define AudioMathOffset_F32_H
 
 #include <arm_math.h>
 #include <AudioStream_F32.h>
@@ -19,14 +19,14 @@ class AudioMathOffset_F32 : public AudioStream_F32
 {
   //GUI: inputs:1, outputs:1  //this line used for automatic generation of GUI node
   public:
-    AudioMathOffset_F32(void) : AudioStream_F32(2, inputQueueArray_f32) {};
-	AudioMathOffset_F32(const AudioSettings_F32 &settings) : AudioStream_F32(2, inputQueueArray_f32) {};
+    AudioMathOffset_F32(void) : AudioStream_F32(1, inputQueueArray_f32) {};
+	AudioMathOffset_F32(const AudioSettings_F32 &settings) : AudioStream_F32(1, inputQueueArray_f32) {};
 	
     void update(void);
     float setOffset(float _offset) { return offset = _offset;}
 	float getOffset(void) { return offset; }
   private:
-    audio_block_f32_t *inputQueueArray_f32[2];
+    audio_block_f32_t *inputQueueArray_f32[1];
 	float32_t offset = 0.0f;
 };
 
