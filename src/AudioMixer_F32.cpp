81aab7537438f420652ba3bc8718fc03a4b2a992
Chip Audette <chipaudette@yahoo.com>
2019-04-02 12:21:48 -0400

    AudioMixer: enable mix even if no chan 0

--- src/AudioMixer_F32.cpp
+++ src/AudioMixer_F32.cpp
@@ -3,24 +3,32 @@
 void AudioMixer4_F32::update(void) {
   audio_block_f32_t *in, *out=NULL;
 
-  out = receiveWritable_f32(0);
-  if (!out) return;
-
-  arm_scale_f32(out->data, multiplier[0], out->data, out->length);
-
-  for (int channel=1; channel < 4; channel++) {
+  //get the first available channel
+  int channel = 0;
+  while  (channel < 4) {
+	  out = receiveWritable_f32(channel);
+	  if (out) break;
+	  channel++;
+  }
+  if (!out) return;  //there was no data available.  so exit.
+  arm_scale_f32(out->data, multiplier[channel], out->data, out->length);  //there was data, so scale it per the mier
+  
+  //add in the remaining channels, as available
+  channel++;
+  while  (channel < 4) {
     in = receiveReadOnly_f32(channel);
-    if (!in) {
-      continue;
-    }
+    if (in) {
+		audio_block_f32_t *tmp = allocate_f32();
 
-    audio_block_f32_t *tmp = allocate_f32();
+		arm_scale_f32(in->data, multiplier[channel], tmp->data, tmp->length);
+		arm_add_f32(out->data, tmp->data, out->data, tmp->length);
 
-    arm_scale_f32(in->data, multiplier[channel], tmp->data, tmp->length);
-    arm_add_f32(out->data, tmp->data, out->data, tmp->length);
-
-    AudioStream_F32::release(tmp);
-    AudioStream_F32::release(in);
+		AudioStream_F32::release(tmp);
+		AudioStream_F32::release(in);
+	} else {
+		//do nothing, this vector is empty
+	}
+	channel++;
   }
 
   if (out) {
