295f14409549309b11664d5db1a7a3a9b64f4151
Chip Audette <chipaudette@yahoo.com>
2019-04-22 12:43:04 -0400

    CalcLevel: add getCurrentLevel

--- src/AudioCalcLevel_F32.h
+++ src/AudioCalcLevel_F32.h
@@ -13,9 +13,13 @@ class AudioCalcLevel_F32 : public AudioFilterTimeWeighting_F32
 		
 		AudioCalcLevel_F32(const AudioSettings_F32 &settings) : AudioFilterTimeWeighting_F32(settings) {}
 		virtual void update(void);
-	
+		virtual float getCurrentLevel(void) { return cur_value; } 
+		virtual float getCurrentLevel_dB(void) { return 10.0f*log10f(cur_value); } 
+		
+		
 	protected:
-	
+		float32_t cur_value = 0.0f;
+
 };
 
 #endif
