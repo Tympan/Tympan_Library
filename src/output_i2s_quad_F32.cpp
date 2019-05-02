6eb4be24c50687f2b2937651fe992c3a43b35afc
Chip Audette <chipaudette@yahoo.com>
2019-05-02 13:02:09 -0400

    Quad I2S Out: cleaner.  samp rate and block size seem to work.

--- src/output_i2s_quad_F32.cpp
+++ src/output_i2s_quad_F32.cpp
@@ -54,7 +54,7 @@ uint32_t  AudioOutputI2SQuad_F32::ch4_offset = 0;
 //audio_block_f32_t * AudioOutputI2SQuad_F32::inputQueueArray[4];
 bool AudioOutputI2SQuad_F32::update_responsibility = false;
 //DMAMEM static uint32_t i2s_tx_buffer[AUDIO_BLOCK_SAMPLES/2*4];  //pack 2 int16s into 1 int32 to make dense, so that 4 channels = 4*(audio_block_samples/2)
-DMAMEM __attribute__((aligned(32))) static uint32_t i2s_tx_buffer[AUDIO_BLOCK_SAMPLES/2*4];
+DMAMEM static uint32_t i2s_tx_buffer[AUDIO_BLOCK_SAMPLES/2*4];
 DMAChannel AudioOutputI2SQuad_F32::dma(false);
 
 //static const uint32_t zerodata[AUDIO_BLOCK_SAMPLES/4] = {0};
@@ -87,8 +87,6 @@ void AudioOutputI2SQuad_F32::begin(void)
 	dma.TCD->SOFF = 2;
 	dma.TCD->ATTR = DMA_TCD_ATTR_SSIZE(1) | DMA_TCD_ATTR_DSIZE(1) | DMA_TCD_ATTR_DMOD(3);
 	dma.TCD->NBYTES_MLNO = 4;
-	Serial.print("sizeof(i2s_tx_buffer) = "); Serial.println(sizeof(i2s_tx_buffer));
-	Serial.print("I2S_BUFFER_TO_USE_BYTES = "); Serial.println(I2S_BUFFER_TO_USE_BYTES);
 	//dma.TCD->SLAST = -sizeof(i2s_tx_buffer); //orig
 	dma.TCD->SLAST = -I2S_BUFFER_TO_USE_BYTES; //allows for variable audio block length
 	dma.TCD->DADDR = &I2S0_TDR0;
@@ -108,7 +106,7 @@ void AudioOutputI2SQuad_F32::begin(void)
 	dma.attachInterrupt(isr);
 	
 	// change the I2S frequencies to make the requested sample rate
-	//AudioOutputI2S_F32::setI2SFreq(AudioOutputI2SQuad_F32::sample_rate_Hz);
+	AudioOutputI2S_F32::setI2SFreq(AudioOutputI2SQuad_F32::sample_rate_Hz);
 	
 	enabled = 1;
 #endif
@@ -128,82 +126,6 @@ void AudioOutputI2SQuad_F32::isr_shuffleDataBlocks(audio_block_f32_t *&block_1st
 	}
 }
 
-int16_t buff1[AUDIO_BLOCK_SAMPLES/2],buff2[AUDIO_BLOCK_SAMPLES/2],buff3[AUDIO_BLOCK_SAMPLES/2],buff4[AUDIO_BLOCK_SAMPLES/2];
-void AudioOutputI2SQuad_F32::isr_orig(void)
-{
-	int16_t *dest; //int16 is data type being sent to the audio codec
-
-	//update the dma and get pointer for the destination tx buffer
-	uint32_t saddr = (uint32_t)(dma.TCD->SADDR);
-	dma.clearInterrupt();
-	//if (saddr < (uint32_t)i2s_tx_buffer + sizeof(i2s_tx_buffer) / 2) { //orig
-	if (saddr < (uint32_t)i2s_tx_buffer + I2S_BUFFER_TO_USE_BYTES / 2) { //variable audio block length
-		// DMA is transmitting the first half of the buffer
-		// so we must fill the second half
-		dest = (int16_t *)&i2s_tx_buffer[AUDIO_BLOCK_SAMPLES]; //orig
-		//dest = (int16_t *)(&i2s_tx_buffer[audio_block_samples]); //new
-		if (AudioOutputI2SQuad_F32::update_responsibility) AudioStream_F32::update_all();
-	} else {
-		dest = (int16_t *)i2s_tx_buffer;
-	}
-
-	//get pointers for source data to copy into the tx buffer
-	const float32_t *src1, *src2, *src3, *src4;
-	const float32_t *zeros = (const float32_t *)zerodata;
-	
-	//if (!block_ch1_1st || !block_ch2_1st || !block_ch3_1st || !block_ch4_1st) {
-	//	Serial.print("Quad: ISR: block was empty: ");
-	//}
-	
-	//src1 = (block_ch1_1st) ? block_ch1_1st->data + ch1_offset : zeros;
-	//src2 = (block_ch2_1st) ? block_ch2_1st->data + ch2_offset : zeros;
-	//src3 = (block_ch3_1st) ? block_ch3_1st->data + ch3_offset : zeros;
-	//src4 = (block_ch4_1st) ? block_ch4_1st->data + ch4_offset : zeros;
-	//if (!block_ch1_1st) { Serial.print("1, "); }
-	src1 = (block_ch1_1st) ? (&(block_ch1_1st->data[ch1_offset])) : zeros;
-	//if (!block_ch2_1st) { Serial.print("2, "); }
-	src2 = (block_ch2_1st) ? (&(block_ch2_1st->data[ch2_offset])) : zeros;
-	//if (!block_ch3_1st) { Serial.print("3, "); }
-	src3 = (block_ch3_1st) ? (&(block_ch3_1st->data[ch3_offset])) : zeros;
-	//src3 = src1;
-	//if (!block_ch4_1st) { Serial.print("4, "); }
-	src4 = (block_ch4_1st) ? (&(block_ch4_1st->data[ch4_offset])) : zeros;
-	//src4 = src2;
-
-	//if (!block_ch1_1st || !block_ch2_1st || !block_ch3_1st || !block_ch4_1st) {
-	//	Serial.println();
-	//}
-	
-	// TODO: fast 4-way interleaved memcpy...
-#if 0
-	for (int i=0; i < audio_block_samples/2; i++) {
-		buff1[i] = (int16_t)(*src1++);
-		buff2[i] = (int16_t)(*src2++);
-		buff3[i] = (int16_t)(*src3++);
-		buff4[i] = (int16_t)(*src4++);
-	}
-	
-	//memcpy_tointerleaveQuad(dest, src1, src2, src3, src4); //this was enabled in the non F32 version
-	memcpy_tointerleaveQuad(dest, buff1, buff2, buff3, buff4); //this was enabled in the non F32 version
-#else
-	//Interleave: note the unexpected order: chan 1, chan 3, chan 2, chan 4
-	//for (int i=0; i < AUDIO_BLOCK_SAMPLES/2; i++) {	//original
-	for (int i=0; i < audio_block_samples/2; i++) {
-		*dest++ = ((int16_t) (*src1++)); //hopefully the float32 src1 data was pre-scaled in the update() method
-		*dest++ = ((int16_t) (*src3++));
-		*dest++ = ((int16_t) (*src2++));
-		*dest++ = ((int16_t) (*src4++));
-	}
-#endif
-
-	//now, shuffle the 1st and 2nd data block for each channel
-	isr_shuffleDataBlocks(block_ch1_1st, block_ch1_2nd, ch1_offset);
-	isr_shuffleDataBlocks(block_ch2_1st, block_ch2_2nd, ch2_offset);
-	isr_shuffleDataBlocks(block_ch3_1st, block_ch3_2nd, ch3_offset);
-	isr_shuffleDataBlocks(block_ch4_1st, block_ch4_2nd, ch4_offset);
-	
-}
-
 void AudioOutputI2SQuad_F32::isr(void)
 {
 	int16_t *dest; //int16 is data type being sent to the audio codec
@@ -221,7 +143,7 @@ void AudioOutputI2SQuad_F32::isr(void)
 		dest = (int16_t *)i2s_tx_buffer;
 	}
 
-	//get pointers for source data to copy into the tx buffer
+	//get pointers for source data that we will copy into the tx buffer
 	const float32_t *src1, *src2, *src3, *src4;
 	const float32_t *zeros = (const float32_t *)zerodata;
 	src1 = (block_ch1_1st) ? (&(block_ch1_1st->data[ch1_offset])) : zeros;
@@ -229,7 +151,7 @@ void AudioOutputI2SQuad_F32::isr(void)
 	src3 = (block_ch3_1st) ? (&(block_ch3_1st->data[ch3_offset])) : zeros;
 	src4 = (block_ch4_1st) ? (&(block_ch4_1st->data[ch4_offset])) : zeros;
 
-	//Interleave: note the unexpected order: chan 1, chan 3, chan 2, chan 4
+	//Interleave and copy into txt buffer (note the unexpected order!!!: chan 1, chan 3, chan 2, chan 4)
 	//for (int i=0; i < AUDIO_BLOCK_SAMPLES/2; i++) {	//original
 	for (int i=0; i < audio_block_samples/2; i++) {
 		*dest++ = (int16_t) (*src1++); //hopefully the float32 src1 data was pre-scaled in the update() method
@@ -271,12 +193,14 @@ void AudioOutputI2SQuad_F32::update_1chan(const int chan,  //this is not changed
 	if (block_f32) {
 		//it is valid.  now process it.
 		
-		/* if (block_f32->length != audio_block_samples) {
-			Serial.print("AudioOutputI2SQuad_F32: *** WARNING ***: audio_block says len = ");
-			Serial.print(block_f32->length);
-			Serial.print(", but I2S settings want it to be = ");
-			Serial.println(audio_block_samples);
-		} */
+		if (chan == 0) {
+			if (block_f32->length != audio_block_samples) {
+				Serial.print("AudioOutputI2SQuad_F32: *** WARNING ***: audio_block says len = ");
+				Serial.print(block_f32->length);
+				Serial.print(", but I2S settings want it to be = ");
+				Serial.println(audio_block_samples);
+			}
+		} 
 	
 		//scale F32 to Int16
 		audio_block_f32_t *block_f32_scaled = AudioStream_F32::allocate_f32();
