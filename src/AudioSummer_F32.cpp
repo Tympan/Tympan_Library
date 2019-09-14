990aa19c11266666c7d60f36524a97176aa46e2e
Chip Audette <chipaudette@yahoo.com>
2019-09-14 19:46:45 -0400

    AudioSummer: do not use disabled channels

--- src/AudioSummer_F32.cpp
+++ src/AudioSummer_F32.cpp
@@ -7,17 +7,25 @@ void AudioSummer4_F32::update(void) {
   int channel = 0;
   while  (channel < 4) {
 	  out = receiveWritable_f32(channel);
-	  if (out) break;
+	  if (out && flag_useChan[channel]) {
+		  //yes, this is an audio block and use it
+		  break;
+	  } else {
+		  //this may or may not be an audio block, but don't use it
+		  if (!out) AudioStream_F32::release(out);	  
+	  }
 	  channel++;
   }
   if (!out) return;  //there was no data available.  so exit.
   
-  //add in the remaining channels, as available
+  //add in the remaining channels, as available and if enabled
   channel++;
   while  (channel < 4) {
     in = receiveReadOnly_f32(channel);
     if (in) {
-		arm_add_f32(out->data, in->data, out->data, in->length);
+		if (flag_useChan[channel]) {
+			arm_add_f32(out->data, in->data, out->data, in->length);
+		}
 		AudioStream_F32::release(in);
 	} else {
 		//do nothing, this vector is empty
