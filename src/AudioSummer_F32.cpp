953c903a0eeffe88926698bf8fd7cd6de545263d
Chip Audette <chipaudette@yahoo.com>
2019-09-14 19:55:51 -0400

    AudioSummer: Add 8 chan version

--- src/AudioSummer_F32.cpp
+++ src/AudioSummer_F32.cpp
@@ -12,7 +12,7 @@ void AudioSummer4_F32::update(void) {
 		  break;
 	  } else {
 		  //this may or may not be an audio block, but don't use it
-		  if (!out) AudioStream_F32::release(out);	  
+		  if (out) AudioStream_F32::release(out);	  
 	  }
 	  channel++;
   }
@@ -38,3 +38,44 @@ void AudioSummer4_F32::update(void) {
     AudioStream_F32::release(out);
   }
 }
+
+
+void AudioSummer8_F32::update(void) {
+  audio_block_f32_t *in, *out=NULL;
+
+  //get the first available channel
+  int channel = 0;
+  while  (channel < 8) {
+	  out = receiveWritable_f32(channel);
+	  if (out && flag_useChan[channel]) {
+		  //yes, this is an audio block and use it
+		  break;
+	  } else {
+		  //this may or may not be an audio block, but don't use it
+		  if (out) AudioStream_F32::release(out);	  
+	  }
+	  channel++;
+  }
+  if (!out) return;  //there was no data available.  so exit.
+  
+  //add in the remaining channels, as available and if enabled
+  channel++;
+  while  (channel < 8) {
+    in = receiveReadOnly_f32(channel);
+    if (in) {
+		if (flag_useChan[channel]) {
+			arm_add_f32(out->data, in->data, out->data, in->length);
+		}
+		AudioStream_F32::release(in);
+	} else {
+		//do nothing, this vector is empty
+	}
+	channel++;
+  }
+
+  if (out) {
+    AudioStream_F32::transmit(out);
+    AudioStream_F32::release(out);
+  }
+}
+
--- src/AudioSummer_F32.h
+++ src/AudioSummer_F32.h
@@ -50,5 +50,37 @@ public:
     bool flag_useChan[4];
 };
 
+class AudioSummer8_F32 : public AudioStream_F32 {
+//GUI: inputs:8, outputs:1  //this line used for automatic generation of GUI node
+//GUI: shortName:Summer4
+public:
+    AudioSummer8_F32() : AudioStream_F32(4, inputQueueArray) { setDefaultValues(); }
+	AudioSummer8_F32(const AudioSettings_F32 &settings) : AudioStream_F32(8, inputQueueArray) { setDefaultValues(); }
+	
+	void setDefaultValues(void) {
+		for (int i=0; i<8; i++) flag_useChan[i] = true;
+	}
+	
+    virtual void update(void);
+
+    int enableChannel(unsigned int channel, bool enable = true) {
+      if ((channel >= 8) || (channel < 0)) return -1;
+      return (int) (flag_useChan[channel] = enable);
+    }
+	void mute(void) { for (int i=0; i < 8; i++) enableChannel(i,false); };  //mute all channels
+	
+	int switchChannel(unsigned int channel) { 
+		//mute all channels except the given one.  Set the given one to 1.0.
+		if ((channel >= 8) || (channel < 0)) return -1;
+		mute(); 
+		enableChannel(channel);
+		return channel;
+	} 
+
+  private:
+    audio_block_f32_t *inputQueueArray[4];
+    bool flag_useChan[8];
+};
+
 
 #endif
