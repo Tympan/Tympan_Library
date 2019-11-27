bbc179cc685d4f63630651789757421358a4b362
Chip Audette <chipaudette@yahoo.com>
2019-11-27 16:47:12 -0500

    AIC3206: maintain knowledge of HP cutoff freq

--- src/control_aic3206.cpp
+++ src/control_aic3206.cpp
@@ -898,9 +898,9 @@ void AudioControlAIC3206::setHPFonADC(bool enable, float cutoff_Hz, float fs_Hz)
 	//see TI application guide Section 2.3.3.1.10.1: http://www.ti.com/lit/an/slaa463b/slaa463b.pdf
 	uint32_t coeff[3];
 	if (enable) {
+		HP_cutoff_Hz = cutoff_Hz; 
 		#if 0
 			//original
-			HP_cutoff_Hz = cutoff_Hz; 
 			sample_rate_Hz = fs_Hz;
 			computeFirstOrderHPCoeff_i32(cutoff_Hz,fs_Hz,(int32_t *)coeff);
 		#else
