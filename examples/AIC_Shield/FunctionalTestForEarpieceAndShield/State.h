22b7b6b5319a197758386454c8d61a19dac35810
Eric Yuan <eyuan@creare.com>
2020-02-21 08:45:52 -0500

    Added Functional Test for Earpiece Shield and PDM Mic
    This code is stripped down to just turn enable the earpiece PDM mic and receiver.  It does not work with the App.

---
+++ examples/AIC_Shield/FunctionalTestForEarpieceAndShield/State.h
@@ -0,0 +1,21 @@
+#ifndef State_h
+#define State_h
+
+enum Mic_Channels {
+  MIC_FRONT_LEFT, 
+  MIC_REAR_LEFT, 
+  MIC_FRONT_RIGHT, 
+  MIC_REAR_RIGHT, 
+  ALL_MICS
+  };
+
+enum Mic_Input {
+  NO_MIC, 
+  INPUT_PCBMICS,
+  INPUT_MICJACK_MIC,
+  INPUT_MICJACK_LINEIN,
+  INPUT_LINEIN_SE,
+  INPUT_PDMMICS
+  };
+
+#endif
