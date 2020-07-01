8886c178a74d48143024e297178994d6eace67ed
Chip Audette <chipaudette@yahoo.com>
2020-07-01 14:56:33 -0400

    AppTut: Basic gain, first commit

---
+++ examples/06-AppTutorial/01-BasicGain/01-BasicGain.ino
@@ -0,0 +1,102 @@
+/*
+*   BasicGain
+*
+*   Created: Chip Audette, Nov 2016
+*   Purpose: Process audio using Tympan by applying gain.
+*
+*   Blue potentiometer adjusts the digital gain applied to the audio signal.
+*
+*   Uses default sample rate of 44100 Hz with Block Size of 128
+*
+*   MIT License.  use at your own risk.
+*/
+
+//here are the libraries that we need
+#include <Tympan_Library.h>  //include the Tympan Library
+
+//create audio library objects for handling the audio
+Tympan                    myTympan(TympanRev::D);  //do TympanRev::D or TympanRev::C
+AudioInputI2S_F32         i2s_in;        //Digital audio *from* the Tympan AIC.
+AudioEffectGain_F32       gain1, gain2;  //Applies digital gain to audio data.
+AudioOutputI2S_F32        i2s_out;       //Digital audio *to* the Tympan AIC.  Always list last to minimize latency
+
+//Make all of the audio connections
+AudioConnection_F32       patchCord1(i2s_in, 0, gain1, 0);    //connect the Left input
+AudioConnection_F32       patchCord2(i2s_in, 1, gain2, 0);    //connect the Right input
+AudioConnection_F32       patchCord11(gain1, 0, i2s_out, 0);  //connect the Left gain to the Left output
+AudioConnection_F32       patchCord12(gain2, 0, i2s_out, 1);  //connect the Right gain to the Right output
+
+
+// define the setup() function, the function that is called once when the device is booting
+const float input_gain_dB = 20.0f; //gain on the microphone
+float vol_knob_gain_dB = 0.0;      //will be overridden by volume knob
+void setup() {
+
+  //begin the serial comms (for debugging)
+  Serial.begin(115200);  delay(500);
+  Serial.println("BasicGain: starting setup()...");
+
+  //allocate the dynamic memory for audio processing blocks
+  AudioMemory_F32(10); 
+
+  //Enable the Tympan to start the audio flowing!
+  myTympan.enable(); // activate the Tympan's audio module
+
+  //Choose the desired input
+  myTympan.inputSelect(TYMPAN_INPUT_ON_BOARD_MIC);     // use the on board microphones
+  //myTympan.inputSelect(TYMPAN_INPUT_JACK_AS_MIC);    // use the microphone jack - defaults to mic bias 2.5V
+  //myTympan.inputSelect(TYMPAN_INPUT_JACK_AS_LINEIN); // use the microphone jack - defaults to mic bias OFF
+
+  //Set the desired volume levels
+  myTympan.volume_dB(0);                   // headphone amplifier.  -63.6 to +24 dB in 0.5dB steps.
+  myTympan.setInputGain_dB(input_gain_dB); // set input volume, 0-47.5dB in 0.5dB setps
+
+  // check the volume knob
+  servicePotentiometer(millis(),0);  //the "0" is not relevant here.
+
+  Serial.println("Setup complete.");
+} //end setup()
+
+
+// define the loop() function, the function that is repeated over and over for the life of the device
+void loop() {
+
+  //check the potentiometer
+  servicePotentiometer(millis(),100); //service the potentiometer every 100 msec
+
+} //end loop();
+
+
+// ///////////////// Servicing routines
+
+//servicePotentiometer: listens to the blue potentiometer and sends the new pot value
+//  to the audio processing algorithm as a control parameter
+void servicePotentiometer(unsigned long curTime_millis, unsigned long updatePeriod_millis) {
+  //static unsigned long updatePeriod_millis = 100; //how many milliseconds between updating the potentiometer reading?
+  static unsigned long lastUpdate_millis = 0;
+  static float prev_val = -1.0;
+
+  //has enough time passed to update everything?
+  if (curTime_millis < lastUpdate_millis) lastUpdate_millis = 0; //handle wrap-around of the clock
+  if ((curTime_millis - lastUpdate_millis) > updatePeriod_millis) { //is it time to update the user interface?
+
+    //read potentiometer
+	float val = float(myTympan.readPotentiometer()) / 1023.0; //0.0 to 1.0
+    val = (1.0/9.0) * (float)((int)(9.0 * val + 0.5)); //quantize so that it doesn't chatter...0 to 1.0
+
+    //send the potentiometer value to your algorithm as a control parameter
+    if (abs(val - prev_val) > 0.05) { //is it different than before?
+      prev_val = val;  //save the value for comparison for the next time around
+
+      //choose the desired gain value based on the knob setting
+      const float min_gain_dB = -20.0, max_gain_dB = 40.0; //set desired gain range
+      vol_knob_gain_dB = min_gain_dB + (max_gain_dB - min_gain_dB)*val; //computed desired gain value in dB
+
+      //command the new gain setting
+      gain1.setGain_dB(vol_knob_gain_dB);  //set the gain of the Left-channel gain processor
+      gain2.setGain_dB(vol_knob_gain_dB);  //set the gain of the Right-channel gain processor
+      Serial.print("servicePotentiometer: Digital Gain dB = "); Serial.println(vol_knob_gain_dB); //print text to Serial port for debugging
+    }
+    lastUpdate_millis = curTime_millis;
+  } // end if
+} //end servicePotentiometer();
