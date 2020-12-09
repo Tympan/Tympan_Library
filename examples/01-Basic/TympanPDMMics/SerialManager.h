98020332891dac82b4d419849aa3f80fc168f38e
Chip Audette <chipaudette@yahoo.com>
2020-12-09 16:50:46 -0500

    TympanPDMMics: Update for latest Tympan_Library

--- examples/01-Basic/TympanPDMMics/SerialManager.h
+++ examples/01-Basic/TympanPDMMics/SerialManager.h
@@ -4,15 +4,31 @@
 
 #include <Tympan_Library.h>
 
-//add in the algorithm whose gains we wish to set via this SerialManager...change this if your gain algorithms class changes names!
-//include "AudioEffectCompWDRC_F32.h"    //change this if you change the name of the algorithm's source code filename
-//typedef AudioEffectCompWDRC_F32 GainAlgorithm_t; //change this if you change the algorithm's class name
+
+//Extern Functions
+extern void setConfiguration(int config);
+extern void togglePrintMemoryAndCPU(void);
+extern void setPrintMemoryAndCPU(bool);
+extern void incrementInputGain(float);
+extern void incrementHeadphoneVol(float);
+
+
+//Extern variables
+extern Tympan myTympan;
+extern AudioSDWriter_F32 audioSDWriter;
+extern float vol_knob_gain_dB;
+extern float input_gain_dB;
+extern const int config_pcb_mics;
+extern const int config_mic_jack;
+extern const int config_line_in_SE;
+extern const int config_line_in_diff;
+extern const int config_pdm_mic;
 
 //now, define the Serial Manager class
 class SerialManager {
   public:
-    SerialManager(TympanBase &_audioHardware,AudioMixer4_F32 &mixer)
-      : audioHardware(_audioHardware), mixerIn(mixer)
+    SerialManager(AudioMixer4_F32 &mixer)
+      : mixerIn(mixer)
         {  };
       
     void respondToByte(char c);
@@ -22,43 +38,25 @@ class SerialManager {
     const float INCREMENT_HEADPHONE_GAIN_DB = 2.5f;  
 
   private:
-    TympanBase &audioHardware;
     AudioMixer4_F32 &mixerIn;
 
 };
 
 void SerialManager::printHelp(void) {
-  audioHardware.println();
-  audioHardware.println("SerialManager Help: Available Commands:");
-  audioHardware.println("   h: Print this help");
-  audioHardware.println("   r: begin recording");
-  audioHardware.println("   s: stop recording");
-  audioHardware.println("   b/n/m/v/d: Switch between (b) on-PCB mic; (n) line-input (single-ended); (m) mic jack; (d) digital PDM mic");
-  audioHardware.print  ("   i/o: Decrease/Increase the Input Gain by "); audioHardware.print(INCREMENT_INPUT_GAIN_DB); audioHardware.println(" dB");
-  audioHardware.print  ("   j/k: Decrease/Increase the Headphone Volume by "); audioHardware.print(INCREMENT_HEADPHONE_GAIN_DB); audioHardware.println(" dB");
-   audioHardware.println("   C: Toggle printing of CPU and Memory usage");
+  myTympan.println();
+  myTympan.println("SerialManager Help: Available Commands:");
+  myTympan.println("   h: Print this help");
+  myTympan.println("   c: Start printing of CPU and Memory usage");
+  myTympan.println("   C: Stop printing of CPU and Memory usage");  
+  myTympan.println("   r: begin recording");
+  myTympan.println("   s: stop recording");
+  myTympan.println("   b/n/m/v/d: Switch between (b) on-PCB mic; (n) line-input (single-ended); (m) mic jack; (d) digital PDM mic");
+  myTympan.print  ("   i/o: Decrease/Increase the Input Gain by "); myTympan.print(INCREMENT_INPUT_GAIN_DB); myTympan.println(" dB");
+  myTympan.print  ("   j/k: Decrease/Increase the Headphone Volume by "); myTympan.print(INCREMENT_HEADPHONE_GAIN_DB); myTympan.println(" dB");
 
-  audioHardware.println();
+
+  myTympan.println();
 }
-
-//Extern Functions
-extern void setConfiguration(int config);
-extern void togglePrintMemoryAndCPU(void);
-//extern void togglePrintAveSignalLevels(bool);
-extern void beginRecordingProcess(void);
-extern void stopRecording(void);
-extern void incrementInputGain(float);
-extern void incrementHeadphoneVol(float);
-
-
-//Extern variables
-extern float vol_knob_gain_dB;
-extern float input_gain_dB;
-extern const int config_pcb_mics;
-extern const int config_mic_jack;
-extern const int config_line_in_SE;
-extern const int config_line_in_diff;
-extern const int config_pdm_mic;
 
 
 
@@ -67,9 +65,16 @@ void SerialManager::respondToByte(char c) {
   switch (c) {
     case 'h': case '?':
       printHelp(); break;
-    case 'C': case 'c':
-      audioHardware.println("Command Received: toggle printing of memory and CPU usage.");
-      togglePrintMemoryAndCPU(); break;
+    case 'c':
+      myTympan.println("Received: start CPU reporting");
+      setPrintMemoryAndCPU(true);
+      //setButtonState("cpuStart",true);
+      break;
+    case 'C':
+      myTympan.println("Received: stop CPU reporting");
+      setPrintMemoryAndCPU(false);
+      //setButtonState("cpuStart",false);
+      break;
     case 'o':
       incrementInputGain(INCREMENT_INPUT_GAIN_DB);
       printGainSettings();
@@ -87,41 +92,53 @@ void SerialManager::respondToByte(char c) {
       printGainSettings();  
       break;
     case 'b':
-      audioHardware.println("Command Received: switch to on-PCB mic; InputGain = 0dB");
+      myTympan.println("Command Received: switch to on-PCB mic; InputGain = 0dB");
       setConfiguration(config_pcb_mics);
       break;
     case 'd':
-      audioHardware.println("Command Received: switch to digital PDM mics; InputGain = 0dB");
+      myTympan.println("Command Received: switch to digital PDM mics; InputGain = 0dB");
       setConfiguration(config_pdm_mic);
       break;
     case 'n':
-      audioHardware.println("Command Received: switch to line-input through-holes (Single-Ended); InputGain = 0dB");
+      myTympan.println("Command Received: switch to line-input through-holes (Single-Ended); InputGain = 0dB");
       setConfiguration(config_line_in_SE);
       break;
     case 'm':
-      audioHardware.println("Command Received: switch to external mic; InputGain = 0dB");
+      myTympan.println("Command Received: switch to external mic; InputGain = 0dB");
       setConfiguration(config_mic_jack);
       break;
     case 'v':
-      audioHardware.println("Command Received: switch to line-input through holes (DIFF); InputGain = 0dB");
+      myTympan.println("Command Received: switch to line-input through holes (DIFF); InputGain = 0dB");
       setConfiguration(config_line_in_diff);
       break;
-    
+   
     case 'r':
-      beginRecordingProcess();
+      myTympan.println("Received: begin SD recording");
+      //beginRecordingProcess();
+      audioSDWriter.startRecording();
+      if (audioSDWriter.getState() == AudioSDWriter_F32::STATE::RECORDING) {
+        //myTympan.print("SD Filename = "); myTympan.println(audioSDWriter.getCurrentFilename());
+      } else {
+        myTympan.print("SD Failed to start recording."); 
+      }
+      
+      //setButtonState("recordStart",true);
       break;
     case 's':
-       stopRecording();
+      myTympan.println("Received: stop SD recording");
+      audioSDWriter.stopRecording();
+      //setButtonState("recordStart",false);
+      break;
     break;
   }
 }
 
 void SerialManager::printGainSettings(void) {
-  audioHardware.print("Vol Knob = "); 
-  audioHardware.print(vol_knob_gain_dB,1);
-  audioHardware.print(", Input PGA = "); 
-  audioHardware.print(input_gain_dB,1);
-  audioHardware.println();
+  myTympan.print("Vol Knob = "); 
+  myTympan.print(vol_knob_gain_dB,1);
+  myTympan.print(", Input PGA = "); 
+  myTympan.print(input_gain_dB,1);
+  myTympan.println();
 }
 
 #endif
