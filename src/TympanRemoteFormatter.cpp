a232c1b56d4a947ba3477db6f31350869807c7d3
Chip Audette <chipaudette@yahoo.com>
2021-08-03 14:01:50 -0400

    TympanRemoteFormatter: expand capacity via using linked lists instead of pre-allocated arrays

---
+++ src/TympanRemoteFormatter.cpp
@@ -0,0 +1,324 @@
+
+#include <TympanRemoteFormatter.h>
+
+String TR_Button::asString(void) {
+	String s;
+	
+	//original method
+	//s = "{'label':'" + label + "','cmd':'" + command + "','id':'" + id + "','width':'" + width + "'}";
+	
+	//new method...don't waste time transmitting fields that don't exist
+	s = "{";
+	if (label.length() > 0) s.concat("'label':'" + label + "',");
+	if (command.length() > 0) s.concat("'cmd':'" + command + "',");
+	if (id.length() > 0) s.concat("'id':'" + id + "',");
+	//if (width.length() > 0) s.concat("'width':'" + width + "'"); //no trailing comma
+	s.concat("'width':'" + String(width) + "'"); //always have a width
+	
+	//if (s.endsWith(",")) s.remove(s.length()-1,1); //strip off trailing comma
+	s.concat("}"); //close off the unit
+	
+	return s;
+}
+
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+TR_Button* TR_Card::addButton(String label, String command, String id, int width) {
+	//make a new button
+	TR_Button *btn = new TR_Button();
+	if (btn==NULL) return btn; //return if we can't allocate it
+	
+	//link it to the previous button (linked list)
+	TR_Button *prevLastButton = lastButton;
+	if (prevLastButton != NULL) prevLastButton->nextButton = btn;
+	
+	//assign the new page to be the new last page
+	lastButton = btn;
+	nButtons++;
+	
+	//add the data
+	btn->label = label;
+	btn->prevButton = prevLastButton;
+	btn->command = command;
+	btn->id = id;
+	btn->width = width;
+
+	//we're done
+	return btn;	
+}
+
+TR_Button* TR_Card::getFirstButton(void) {
+	TR_Button *btn = lastButton;
+	if (btn == NULL) return NULL;
+
+	//keep stepping back to find the first
+	TR_Button *prev_btn = btn->prevButton;
+	while (prev_btn != NULL) {
+		btn = prev_btn;
+		prev_btn = btn->prevButton;
+	}
+	return btn;
+}
+
+String TR_Card::asString(void) {
+	String s;
+
+	s = "{'name':'"+name+"'";
+	
+	//write buttons
+	TR_Button* btn = getFirstButton();
+	if (btn != NULL) {
+		//write the first button
+		s += ",'buttons':[";
+		s += btn->asString();
+
+		//write other buttons
+		TR_Button* next_button = btn->nextButton;
+		while (next_button != NULL) {
+			btn = next_button;
+			s += ",";
+			s += btn->asString();        
+			next_button = btn->nextButton;
+		}
+		
+		//finish the list
+		s += "]";        
+	}
+	
+	//close out
+	s += "}";
+
+	return s;
+}
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TR_Card* TR_Page::addCard(String name) {
+
+	//make a new card
+	TR_Card *card = new TR_Card();
+	if (card==NULL) return card; //return if we can't allocate it
+	
+	//link it to the previous card (linked list)
+	TR_Card *prevLastCard = lastCard;
+	if (prevLastCard != NULL) prevLastCard->nextCard = card;
+	
+	//assign the new page to be the new last page
+	lastCard = card;
+	nCards++;
+	
+	//add the data
+	card->name = name;
+	card->prevCard = prevLastCard;
+
+	//we're done
+	return card;
+}
+
+TR_Card* TR_Page::getFirstCard(void) {
+	TR_Card *card = lastCard;
+	if (card == NULL) return NULL;
+	
+	//keep stepping back to find the first
+	TR_Card *prev_card = card->prevCard;
+	while (prev_card != NULL) {
+		card = prev_card;
+		prev_card = card->prevCard;
+	}
+	return card;
+}
+
+String TR_Page::asString(void) {
+	String s;
+
+	s = "{'title':'"+name+"'";
+	
+	//write cards
+	TR_Card* card = getFirstCard();
+	if (card != NULL) {
+		//write first card
+		s += ",'cards':[";
+		s += card->asString();
+
+		//write additional cards
+		TR_Card *next_card = card->nextCard;
+		while (next_card != NULL) {
+			card = next_card;
+			s += ",";
+			s += card->asString();        
+			next_card = card->nextCard;
+		}
+	
+		//finish the list
+		s += "]"; 
+	}
+	
+	//close out
+	s += "}";
+
+	return s;
+}
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TR_Page* TympanRemoteFormatter::addPage(String name) {
+	
+	//make a new page
+	TR_Page *page = new TR_Page();
+	if (page==NULL) return page; //return if we can't allocate it
+	
+	//link it to the previous page (linked list)
+	TR_Page *prevLastPage = lastPage;
+	if (prevLastPage != NULL) prevLastPage->nextPage = page;
+	
+	//assign the new page to be the new last page
+	lastPage = page;
+	nCustomPages++;
+	
+	//add the data
+	page->name = name;
+	page->prevPage = prevLastPage;
+	
+	//we're done!
+	return page;
+} 
+
+
+void TympanRemoteFormatter::addPredefinedPage(String s) {
+	if (predefPages.length()>0) {
+		predefPages += ",";
+	}
+	predefPages += "'"+s+"'";
+	nPredefinedPages++;
+}
+
+
+TR_Page* TympanRemoteFormatter::getFirstPage(void) {
+	TR_Page *page = lastPage;
+	if (page == NULL) return NULL;
+	
+	//keep stepping back to find the first
+	TR_Page *prev_page = page->prevPage;
+	while (prev_page != NULL) {
+		page = prev_page;
+		prev_page = page->prevPage;
+	}
+	return page;
+}
+
+/* int TympanRemoteFormatter::c_str(char *c_str, int maxLen) {
+	int dest=0, source=0;
+	String s;
+	
+	s = String(F("JSON={'icon':'tympan.png',"));
+	source = 0;
+	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
+	
+	// Add pages:
+	if (nCustomPages > 0) {
+		s = String("'pages':[") + pages[0].asString();
+		source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
+	}
+	for (int i=1; i<nCustomPages; i++) {
+		s  = String(",") + pages[i].asString();
+		source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
+	}
+	if (nCustomPages > 0) {
+		s = String("]");        
+		source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
+	}
+	
+	// Add predefined pages:
+	if (predefPages.length()>1) {
+		if (dest>0) {
+			s = String(",");        
+			source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
+		}
+		s = String(F("'prescription':{'type':'BoysTown','pages':[")) + predefPages + String("]}");
+		source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
+	}
+	s = String("}");
+	source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
+	
+	dest = min(dest,maxLen);
+	c_str[dest] = '\0';  //null terminated
+	return dest;
+} */
+
+
+String TympanRemoteFormatter::asString(void)  {
+	String s;
+
+	s = F("JSON={'icon':'tympan.png',");
+
+
+	// Add pages
+	s += "'pages':[";
+	bool anyCustomPages = false;  //we don't know yet
+	TR_Page* page = getFirstPage();
+	if (page != NULL) {
+		anyCustomPages = true;
+		
+		//write the first page
+		s += page->asString();
+
+
+		//add subsequent pages
+		TR_Page* next_page = page->nextPage;
+		while (next_page != NULL) {
+
+			page = next_page;
+			s += ",";
+			s += page->asString();        
+			next_page = page->nextPage;
+		} 
+	}
+	//close out the custom pages
+	s += "]";     
+	
+	// Add predefined pages:
+	if (predefPages.length()>1) {
+		//if (s.length()>1) {
+			s += ",";        
+		//}
+		
+		s += F("'prescription':{'type':'BoysTown','pages':[") + predefPages +"]}";
+	}
+	
+	//close out
+	s += "}";
+
+	return s;      
+}
+      
+void TympanRemoteFormatter::asValidJSON(void) {
+	String s;
+	s = this->asString();
+	s.replace("'","\"");
+	Serial.println(F("Pretty printed (a.k.a. valid json):"));
+	Serial.println(s);
+}	  
+
+
+void TympanRemoteFormatter::deleteAllPages(void) {
+	//loop through and delete each page that was created
+	TR_Page *page = lastPage;
+	while (page != NULL) {
+		TR_Page *prev_page = page->prevPage; //look to the next loopo
+		delete page;  //delete the current page
+		nCustomPages--; //decrement the count
+
+		page = prev_page; //prepare for next loop
+		if (page != NULL) page->nextPage = NULL; //there is no more next page
+		lastPage = page; //update what the new lastPage is
+	}
+	
+	//clear out the predefined pages
+	predefPages = String("");
+	nPredefinedPages = 0;
+	
+}
--- src/TympanRemoteFormatter.h
+++ src/TympanRemoteFormatter.h
@@ -3,7 +3,9 @@
 
 #define TR_MAX_N_PAGES 10
 #define TR_MAX_N_CARDS 10
-#define TR_MAX_N_BUTTONS 10
+#define TR_MAX_N_BUTTONS 12
+
+#include <Arduino.h>
 
 class TR_Button {
   public:
@@ -12,86 +14,71 @@ class TR_Button {
     String id;
     int width;
 
-    TR_Button() {
-    }
+    TR_Button() {}
 
-    String asString() {
-		String s;
-		
-		//original method
-		//s = "{'label':'" + label + "','cmd':'" + command + "','id':'" + id + "','width':'" + width + "'}";
-		
-		//new method...don't waste time transmitting fields that don't exist
-		s = "{";
-		if (label.length() > 0) s.concat("'label':'" + label + "',");
-		if (command.length() > 0) s.concat("'cmd':'" + command + "',");
-		if (id.length() > 0) s.concat("'id':'" + id + "',");
-		//if (width.length() > 0) s.concat("'width':'" + width + "'"); //no trailing comma
-		s.concat("'width':'" + String(width) + "'"); //always have a width
-		
-		//if (s.endsWith(",")) s.remove(s.length()-1,1); //strip off trailing comma
-		s.concat("}"); //close off the unit
-		
-		return s;
-    }
+    String asString();
+	
+	TR_Button *prevButton = NULL;  //for linked list
+	TR_Button *nextButton = NULL;  //for linked list
 };
+
 
 class TR_Card {
   
   public:
     String name = "";
 
-    TR_Card() {
-      nButtons = 0;
-    }
+    TR_Card(void) { }
+	~TR_Card(void) {
+		//loop through and delete each button that was created
+		TR_Button *button = lastButton;
+		while (button != NULL) {
+			TR_Button *prev_button = button->prevButton; //look to the next loop
+			delete button;   //delete the button
+			nButtons--;		 //decrement the count
+			
+			button = prev_button; //prepare for the next loop
+			if (button != NULL) button->nextButton = NULL;  //there is no more next button
+			lastButton = button;  //update what the new lastButton is
+		}
+	}
 
-    void addButton(String label, String command, String id, int width) {
-      TR_Button *btn;
+    TR_Button* addButton(String label, String command, String id, int width);
+	TR_Button* getFirstButton(void);
+	
+    String asString(void);
 
-	  if (nButtons >= TR_MAX_N_BUTTONS) return; //too many buttons already!
-      btn = &(buttons[nButtons]);
-      btn->label = label;
-      btn->command = command;
-      btn->id = id;
-      btn->width = width;
-
-      nButtons++;
-    }
-
-    String asString() {
-      String s;
-      int i;
-      
-      s = "{'name':'"+name+"'";
-      if (nButtons > 0) {
-        s += ",'buttons':[";
-        s += buttons[0].asString();
-      }
-      for (i=1; i<nButtons; i++) {
-        s += ",";
-        s += buttons[i].asString();        
-      }
-      if (nButtons > 0) {
-        s += "]";        
-      }
-      s += "}";
-
-      return s;
-    }
+	//TR_Button *firstButton = NULL;
+	TR_Button *lastButton = NULL;   //for linked list
+	TR_Card *prevCard = NULL;       //for linked list
+	TR_Card *nextCard = NULL; 		//for linked list
 
   private:
-    TR_Button buttons[TR_MAX_N_BUTTONS];
-    int nButtons;
+    //TR_Button buttons[TR_MAX_N_BUTTONS];
+    int nButtons = 0;
 };
+
 
 class TR_Page {
   public:
 
     String name = "";
     
-    TR_Page() {
-      nCards = 0;
-    }
+    TR_Page(void) {}
+	~TR_Page(void) {
+		//loop through and delete each card that was created
+		TR_Card *card = lastCard;
+		while (card != NULL) {
+			TR_Card *prev_card = card->prevCard; //look to the next loop
+			delete card;  //delete the card
+			nCards--;     //decrement the count
+			
+			card = prev_card; //prepare for next loop
+			if (card != NULL) card->nextCard = NULL;  //there is no more next card
+			lastCard = card;  //update what the new lastCard is
+		}
+	}
+
 
 //	TR_Card *addCard(TR_Card *_inCard) {
 //		if (nCards >= TR_MAX_N_CARDS) return NULL;  //too many already!
@@ -99,48 +86,28 @@ class TR_Page {
 //		nCards++; //increment to next card
 //		return _inCard;
 //	}
-    TR_Card *addCard(String name) {
-		if (nCards >= TR_MAX_N_CARDS) return NULL;  //too many already!
-		TR_Card *card;
 
-		card = &(cards[nCards]);
-		card->name = name;
-		nCards++;
-
-		return card;
-    }
-
-    String asString() {
-      String s;
-      int i;
-      
-      s = "{'title':'"+name+"'";
-      if (nCards > 0) {
-        s += ",'cards':[";
-        s += cards[0].asString();
-      }
-      for (i=1; i<nCards; i++) {
-        s += ",";
-        s += cards[i].asString();        
-      }
-      if (nCards > 0) {
-        s += "]";        
-      }
-      s += "}";
-      
-      return s;
-    }
-
+    
+	TR_Card* addCard(String name);
+	TR_Card* getFirstCard(void);
+	
+    String asString(void);
+	
+	//TR_Card *firstCard = NULL;
+	TR_Card *lastCard = NULL;   //for linked list
+	TR_Page *prevPage = NULL;   //for linked list
+	TR_Page *nextPage = NULL;   //for linked list
+	
   private:
-    TR_Card cards[TR_MAX_N_CARDS];
-    int nCards;
+    //TR_Card cards[TR_MAX_N_CARDS];
+    int nCards = 0;
 };
+
 
 class TympanRemoteFormatter {
   public:
-    TympanRemoteFormatter(void) {
-      nPages = 0;
-    }
+    TympanRemoteFormatter(void) { }
+	~TympanRemoteFormatter(void) { deleteAllPages(); }
 
 //	TR_Page* addPage(TR_Page *_inPage) {
 //		if (nPages >= TR_MAX_N_PAGES)  return NULL;
@@ -148,105 +115,27 @@ class TympanRemoteFormatter {
 //		nPages++;
 //		return _inPage;
 //	}
-    TR_Page* addPage(String name) {
-		if (nPages >= TR_MAX_N_PAGES)  return NULL;
-		TR_Page *page;
-		page = &(pages[nPages]);
-		page->name = name;
-		nPages++;
-		return page;
-    } 
 
-    void addPredefinedPage(String s) {
-      if (predefPages.length()>0) {
-        predefPages += ",";
-      }
-      predefPages += "'"+s+"'";
-    }
+    TR_Page* addPage(String name);
+    void addPredefinedPage(String s);
+	TR_Page* getFirstPage(void);
 	
-	int c_str(char *c_str, int maxLen) {
-		int dest=0, source=0;
-		String s;
-		
-		s = String(F("JSON={'icon':'tympan.png',"));
-		source = 0;
-		while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
-		
-		// Add pages:
-		if (nPages > 0) {
-			s = String("'pages':[") + pages[0].asString();
-			source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
-		}
-		for (int i=1; i<nPages; i++) {
-			s  = String(",") + pages[i].asString();
-			source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
-		}
-		if (nPages > 0) {
-			s = String("]");        
-			source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
-		}
-		
-		// Add predefined pages:
-		if (predefPages.length()>1) {
-			if (dest>0) {
-				s = String(",");        
-				source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
-			}
-			s = String(F("'prescription':{'type':'BoysTown','pages':[")) + predefPages + String("]}");
-			source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
-		}
-		s = String("}");
-		source = 0;	while ((dest < maxLen) && (source < (int)s.length())) c_str[dest++] = s[source++];
-		
-		dest = min(dest,maxLen);
-		c_str[dest] = '\0';  //null terminated
-		return dest;
-	}
-    
-    String asString()  {
-      String s;
-      int i;
+	//int c_str(char *c_str, int maxLen);
+    String asString(void);
+    void asValidJSON(void);
 
-      s = F("JSON={'icon':'tympan.png',");
-
-      // Add pages:
-      if (nPages > 0) {
-        s += "'pages':[";
-        s += pages[0].asString();
-      }
-      for (i=1; i<nPages; i++) {
-        s += ",";
-        s += pages[i].asString();        
-      }
-      if (nPages > 0) {
-        s += "]";        
-      }
-      // Add predefined pages:
-      if (predefPages.length()>1) {
-        if (s.length()>1) {
-          s += ",";        
-        }
-        s += F("'prescription':{'type':'BoysTown','pages':[") + predefPages +"]}";
-      }
-      s += "}";
-      
-      return s;      
-    }
-
-    void asValidJSON() {
-      String s;
-      s = this->asString();
-      s.replace("'","\"");
-      Serial.println(F("Pretty printed (a.k.a. valid json):"));
-      Serial.println(s);
-    }
-
-	int get_nPages(void) { return nPages; }
+	int get_nPages(void) { return nCustomPages + nPredefinedPages; }
+	void deleteAllPages(void);
+	
+	//TR_Page *firstPage = NULL;
+	TR_Page *lastPage = NULL;    //for linked list
 
   private:
-    TR_Page pages[TR_MAX_N_PAGES];
-    int nPages;
-    String predefPages;
+    //TR_Page pages[TR_MAX_N_PAGES];
+    //int nPages = 0;
+    int nCustomPages = 0;
+	int nPredefinedPages = 0;
+	String predefPages;
 };
 
 
