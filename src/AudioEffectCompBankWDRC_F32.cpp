7a4583358bf93b03551acf71a58d007cb94ce5dd
Chip Audette <chipaudette@yahoo.com>
2021-08-10 18:06:28 -0400

    AudioEffectCompBankWDRC: vectorized.  compiles.  doesn't work.

--- src/AudioEffectCompBankWDRC_F32.cpp
+++ src/AudioEffectCompBankWDRC_F32.cpp
@@ -20,59 +20,31 @@
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// This method sets the value of the number of compressors being employed, as being tracked by this state-tracking class.
-// This method doesn't change the algorithm's actual audio processing.
-// To change the actual audio processing, do AudioEffectCompBankWDRC_F32::set_n_chan()
-int AudioCompBankStateWDRC_F32::set_n_chan(int n) {
-	if (compressors == NULL) return 0;
-	if ((n < 0) || (n > max_n_chan)) return -1; //this is an error
-	n_chan = n;
-	return n_chan; 
-}
-
-/* //This method is purely internal to this state-tracking class.  It sets the size of the arrays used to hold
-//the tracking of states for each compressor.  It must be equal to or larger than the actual number of channels
-//tracked.
-int AudioCompBankStateWDRC_F32::set_max_n_chan(int n) {
-	//check inputs
-	if ((n < 1) || (n > 64)) return -1; //-1 is an error
-	
-	//compare to current setting
-	if (n <= max_n_chan) return 0;  //nothing to do, so return...zero is OK
-	
-	//we do need to expand our arrays, so first delete the current arrays
-	//
-	// <TO DO!!!!>   Example: if (crossover_freq_Hz != NULL) delete crossover_freq_Hz;  //delete arrays here
-	//
-	max_n_chan = 0; n_chan = 0;  //reset our tracking of the sizes
-	
-	//create new arrays
-	//
-	// <TO DO!!!!>   Example: crossover_freq_Hz = new float[n]; //create new arrays here
-	//
-	
-	//return with error if memory allocation failed
-	//
-	// <TO DO!!!!>   Example: if (crossover_freq_Hz == NULL) return -1; //return if memory can't be allocated (-1 is an error)
-	//
-		
-	//looks good!  let's return
-	max_n_chan = n;
-	return 0;  //zero is OK;
-} */
 
 //Tell this state-tracking class which compressors to reference when asking for the different
 //state parameter values (so as we don't actually need to keep a local copy of each and every
 //parameter of the compressors)
-void AudioCompBankStateWDRC_F32::setCompressors(int n, AudioEffectCompWDRC_F32 c[]) {
-	if (max_n_chan != n) {
-		if (compressors != NULL) delete compressors;  //delete the pointer array that we allocated
-		max_n_chan = n;
-		compressors = new AudioEffectCompWDRC_F32*[max_n_chan];
-	}
+void AudioCompBankStateWDRC_F32::setCompressors(std::vector<AudioEffectCompWDRC_F32> &c) {
+		
+	//make big enough
+	compressors.resize(c.size());
+	compressors.shrink_to_fit();
+	
+	//add new compressors
+	for (int i=0; i < (int)c.size(); i++) compressors[i] = &(c[i]); //get pointer
+	
+	//shrink n_chan, if it overshoots past the end of our new array
+	if (get_n_chan() > get_max_n_chan()) set_n_chan(get_max_n_chan());
+}
 
-	for (int i=0; i<n; i++) compressors[i] = &(c[i]); //make an array of pointers to each instance of the class
-	if (get_n_chan() > max_n_chan) set_n_chan(max_n_chan);
+// This method sets the value of the number of compressors being employed, as being tracked by this state-tracking class.
+// This method doesn't change the algorithm's actual audio processing.
+// To change the actual audio processing, do AudioEffectCompBankWDRC_F32::set_n_chan()
+int AudioCompBankStateWDRC_F32::set_n_chan(int n) {
+	if (compressors.size() == 0) return 0;
+	if ((n < 0) || (n > get_max_n_chan())) return -1; //this is an error
+	n_chan = n;
+	return n_chan; 
 }
 
 
@@ -84,19 +56,38 @@ void AudioCompBankStateWDRC_F32::setCompressors(int n, AudioEffectCompWDRC_F32 c
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+int AudioEffectCompBankWDRC_F32::set_max_n_chan(int n_max_chan) {
+
+	compressors.resize(n_max_chan);
+	compressors.shrink_to_fit();
+	int new_max_n_size = (int)compressors.size();
+	
+	state.setCompressors(compressors);
+	if (new_max_n_size < get_n_chan()) set_n_chan(new_max_n_size);
+	return (int)compressors.size();
+}
+
 
 int AudioEffectCompBankWDRC_F32::configureFromDSLandGHA(float fs_Hz, const BTNRH_WDRC::CHA_DSL &this_dsl, const BTNRH_WDRC::CHA_WDRC &this_gha) {
 	
 	//sorta check the validity of inputs
 	int n_chan_to_load = this_dsl.nchannel;
+	if ((n_chan_to_load < 1) || (n_chan_to_load >= __MAX_NUM_COMP)) {
+		Serial.println(F("AudioEffectCompBankWDRC_F32: configureFromDSLandGHA: *** ERROR ***"));
+		Serial.println(F("    : dsl.nchannel = ") + String(n_chan_to_load) + " must be 1 to " + String(__MAX_NUM_COMP));
+		Serial.println(F("    : No change to WDRC configurations.  Returning."));
+		return -1;  //-1 is error
+	}
+
+	//ensure that we have space
+	if (n_chan_to_load > state.get_max_n_chan()) set_max_n_chan(n_chan_to_load);
 	int max_n_chan = state.get_max_n_chan();
 	if (n_chan_to_load > max_n_chan) {
-		Serial.println(F("AudioEffectCompBankWDRC_F32: configureFromDSLandGHA: *** ERROR ***: dsl.nchannel > max_n_chan"));
-		Serial.print(F("    : dsl.nchannel = ")); Serial.println(this_dsl.nchannel);
-		Serial.print(F("    : max_n_chan = ")); Serial.println(max_n_chan);
-		Serial.println(F("    : Loading only up to max_n_chan.  Continuing..."));
+		Serial.println(F("AudioEffectCompBankWDRC_F32: configureFromDSLandGHA: *** ERROR ***"));
+		Serial.println(F("    : cannot allocate enough memory to fit all ") + String(n_chan_to_load) + F(" requested channels."));
+		Serial.println(F("    : Continuing using only ") + String(max_n_chan) + F(" channels..."));
 		n_chan_to_load = max_n_chan;
-	} 
+	}
 	
 	//parse the inputs that apply to all channels [logic and values are extracted from from CHAPRO repo agc_prepare.c]
 	float atk = (float)this_dsl.attack;   //milliseconds!
@@ -140,12 +131,12 @@ void AudioEffectCompBankWDRC_F32::update(void) {
 		 //request the in-coming data block
 		audio_block_f32_t *block = AudioStream_F32::receiveReadOnly_f32(Ichan);
 		
-		if (block) { //did we get a block of data?
+		if (block != NULL) { //did we get a block of data?
 		
 			//request a data block to hold th processed data
 			audio_block_f32_t *out_block = AudioStream_F32::allocate_f32();
 			
-			if (out_block) { //did we get a valid memory handle?
+			if (out_block != NULL) { //did we get a valid memory handle?
 				//do the algorithm
 				int is_error = compressors[Ichan].processAudioBlock(block,out_block); //anything other than a zero is an error
 				
@@ -161,9 +152,9 @@ void AudioEffectCompBankWDRC_F32::update(void) {
 }
 
 int AudioEffectCompBankWDRC_F32::set_n_chan(int val) {
-	val = min(val, __MAX_NUM_COMP); 
+	val = min(val, state.get_max_n_chan()); 
 	int n_chan = state.set_n_chan(val);
-	for (int Ichan = 0; Ichan < __MAX_NUM_COMP; Ichan++) {
+	for (int Ichan = 0; Ichan < (int)compressors.size(); Ichan++) {
 		if (Ichan < n_chan) {
 			//compressors[Ichan].enable(true);  //enable the individual compressor [there is no such method?]
 		} else {
--- src/AudioEffectCompBankWDRC_F32.h
+++ src/AudioEffectCompBankWDRC_F32.h
@@ -21,15 +21,15 @@
 #include <AudioEffectCompWDRC_F32.h>	  //from Tympan_Library
 #include <SerialManager_UI.h>			  //from Tympan_Library
 #include <TympanRemoteFormatter.h> 		  //from Tympan_Library
+#include <vector>
 
-#define __MAX_NUM_COMP 8    //maximum number of compressors to allow
+#define __MAX_NUM_COMP 24  //max number of compressors.  Limited by number fo inputs that we enable for the Audio Library connections
 
 //This class helps manage some of the configuration and state information of the AudioEffectCompWDRC classes.
 //It is also helpful for managing the GUI on the TympanRemote mobile App.
 class AudioCompBankStateWDRC_F32 {
 	public:
-		AudioCompBankStateWDRC_F32(void) {}; //nothing to do in the constructor
-		~AudioCompBankStateWDRC_F32(void) { if ( compressors != NULL) delete compressors; };
+		AudioCompBankStateWDRC_F32(void) {}; //nothing to do in the constructor;
 	
 		float sample_rate_Hz = 0.0f;
 
@@ -39,8 +39,8 @@ class AudioCompBankStateWDRC_F32 {
 		
 		//keep track of the maximum number of filters...the user shouldn't have to worry about this
 		//int set_max_n_chan(int n);
-		void setCompressors(int n, AudioEffectCompWDRC_F32 c[]); //also defines max_n_chan
-		int get_max_n_chan(void) { return max_n_chan; }
+		void setCompressors(std::vector<AudioEffectCompWDRC_F32> &c); //also defines max_n_chan
+		int get_max_n_chan(void) { return compressors.size(); }
 		
 		//get parameter values from the compressors
 		float getAttack_msec(int i=0) { if (i < get_n_chan()) { return compressors[i]->getAttack_msec(); } else { return 0.0f; }};
@@ -54,12 +54,9 @@ class AudioCompBankStateWDRC_F32 {
 		float getKneeLimiter_dBSPL(int i=0) { if (i < get_n_chan()) { return compressors[i]->getKneeLimiter_dBSPL(); } else { return 0.0f; }};
 
 	protected:
-		AudioEffectCompWDRC_F32** compressors;  //will be an array of pointers to our compressors
-		int max_n_chan = 0;  //should correspond to the length of the array of compressors
 		int n_chan = 0;      //should correspond to however many of the filters are actually being employed by the AuioFilterbank
 		//float *crossover_freq_Hz;  //this really only needs to be [nfilters-1] in length, but we'll generally allocate [nfilters] just to avoid mistaken overruns
-
-
+		std::vector<AudioEffectCompWDRC_F32 *> compressors;  //this really only needs to be [max_n_filters-1] in length, but we'll generally allocate [max_n_filters] just to avoid mistaken overruns
 	
 };
 
@@ -69,27 +66,27 @@ class AudioEffectCompBankWDRC_F32 : public AudioStream_F32 {
 //GUI: inputs:8, outputs:8  //this line used for automatic generation of GUI node  
 //GUI: shortName:CompBankWDRC
 	public:
-		AudioEffectCompBankWDRC_F32(void): AudioStream_F32(__MAX_NUM_COMP,inputQueueArray) { 
-			setSampleRate_Hz(compressors[0].getSampleRate_Hz());
-			state.setCompressors(__MAX_NUM_COMP,compressors); //initilize
-		} 
+		AudioEffectCompBankWDRC_F32(void): AudioStream_F32(__MAX_NUM_COMP,inputQueueArray) { }
+		
 		AudioEffectCompBankWDRC_F32(const AudioSettings_F32 &settings) : AudioStream_F32(__MAX_NUM_COMP,inputQueueArray) {
 			setSampleRate_Hz(settings.sample_rate_Hz);
-			state.setCompressors(__MAX_NUM_COMP,compressors); //initilize
+			//audio_settings_ptr = &settings;
 		}
-	
 	
 		//define the update() function, which is called automatically every time there is a new audio block
 		virtual void update(void);
 	
 		//set number of channels
+		int set_max_n_chan(int n_max_chan);
 		int set_n_chan(int n);
-		int get_n_nchan(void) { return state.get_n_chan(); }
+		int get_n_chan(void) { return state.get_n_chan(); }
 			
 		//set sample rate 
 		float setSampleRate_Hz(const float _fs_Hz) {
-			for (int i=0; i< __MAX_NUM_COMP; i++) {
-				state.sample_rate_Hz = compressors[i].setSampleRate_Hz(_fs_Hz);
+			if (compressors.size() > 0) {
+				for (int i=0; i < (int)compressors.size(); i++) {
+					state.sample_rate_Hz = compressors[i].setSampleRate_Hz(_fs_Hz);
+				}
 			}
 			return state.sample_rate_Hz;
 		}
@@ -101,11 +98,13 @@ class AudioEffectCompBankWDRC_F32 : public AudioStream_F32 {
 		AudioCompBankStateWDRC_F32 state;
 	
 		//here are the compressors...replace with a vector?
-		AudioEffectCompWDRC_F32 compressors[__MAX_NUM_COMP];
+		//AudioEffectCompWDRC_F32 compressors[__MAX_NUM_COMP];
+		std::vector<AudioEffectCompWDRC_F32> compressors;
+		
 	protected:
 		audio_block_f32_t *inputQueueArray[__MAX_NUM_COMP];  //required as part of AudioStream_F32.  One input.
 		bool is_enabled = false;
-			
+		//AudioSettings_F32 *audio_settings_ptr = NULL;
 };
 
 #endif
