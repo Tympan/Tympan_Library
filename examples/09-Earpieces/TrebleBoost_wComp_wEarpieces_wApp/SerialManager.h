33a0f804e94ba44704263597153dc798a32ec087
Chip Audette <chipaudette@yahoo.com>
2021-08-19 12:48:45 -0400

    Earpiece Examples: replace Earpieces_TrebBoostComp_wApp with TrebleBoost_wComp_wEarpieces_wApp

---
+++ examples/09-Earpieces/TrebleBoost_wComp_wEarpieces_wApp/SerialManager.h
@@ -0,0 +1,196 @@
+
+#ifndef _SerialManager_h
+#define _SerialManager_h
+
+#include <Tympan_Library.h>
+#include "State.h"
+
+//classes from the main sketch that might be used here
+extern Tympan myTympan;                    //created in the main *.ino file
+extern State myState;                      //created in the main *.ino file
+extern EarpieceMixer_F32_UI earpieceMixer; //created in the main *.ino file
+
+//functions in the main sketch that I want to call from here
+extern void incrementHighpassFilters(float);
+extern void printHighpassCutoff(void);
+
+//
+// The purpose of this class is to be a central place to handle all of the interactions
+// to and from the SerialMonitor or TympanRemote App.  Therefore, this class does things like:
+//
+//    * Define what buttons and whatnot are in the TympanRemote App GUI
+//    * Define what commands that your system will respond to, whether from the SerialMonitor or from the GUI
+//    * Send updates to the GUI based on the changing state of the system
+//
+// You could achieve all of these goals without creating a dedicated class.  But, it 
+// is good practice to try to encapsulate some of these functions so that, when the
+// rest of your code calls functions related to the serial communciation (USB or App),
+// you have a better idea of where to look for the code and what other code it relates to.
+//
+
+//now, define the Serial Manager class
+class SerialManager : public SerialManagerBase  {  // see Tympan_Library for SerialManagerBase for more functions!
+  public:
+    SerialManager(BLE *_ble) : SerialManagerBase(_ble) {};
+      
+    void printHelp(void);
+    void createTympanRemoteLayout(void); 
+    void printTympanRemoteLayout(void); 
+    bool processCharacter(char c);  //this is called automatically by SerialManagerBase.respondToByte(char c)
+
+    //method for updating the GUI on the App
+    void setFullGUIState(bool activeButtonsOnly = false);
+    void updateGainDisplay(void);
+    void updateFilterDisplay(void);
+    void updateCpuDisplayOnOff(void);
+    void updateCpuDisplayUsage(void);
+    void sendDataToAppSerialPlotter(const String data1, const String data2);
+
+    //factors by which to raise or lower the parameters when receiving commands from TympanRemote App
+    float freqIncrement = powf(2.0,1.0/3.0);  //raise or lower by 1/3rd octave
+  private:
+
+    TympanRemoteFormatter myGUI;  //Creates the GUI-writing class for interacting with TympanRemote App
+   
+};
+
+void SerialManager::printHelp(void) {  
+  Serial.println("SerialManager Help: Available Commands:");
+  Serial.println(" h: Print this help");
+  Serial.println(" t/T: Incr/Decrease Cutoff of Highpass Filter");
+  Serial.println(" ],}: Start/stop sending data to the App SerialPlotter");
+
+  //Add in the printHelp() that is built-into the other UI-enabled system components.
+  //The function call below loops through all of the UI-enabled classes that were
+  //attached to the serialManager in the setupSerialManager() function used back
+  //in the main *.ino file.
+  SerialManagerBase::printHelp();  ////in here, it automatically loops over the different UI elements issuing printHelp()
+  
+  Serial.println();
+}
+
+//switch yard to determine the desired action...this method is much shorter now that we're using a lot
+//of logic that has already been built-into the UI-enabled classes.
+bool SerialManager::processCharacter(char c) {  //this is called by SerialManagerBase.respondToByte(char c)
+  bool ret_val = true;
+
+  switch (c) {
+    case 'h':
+      printHelp(); 
+      break;
+    case 'J': case 'j':           //The TympanRemote app sends a 'J' to the Tympan when it connects
+      printTympanRemoteLayout();  //in resonse, the Tympan sends the definition of the GUI that we'd like
+      break;
+    case 't':
+      incrementHighpassFilters(freqIncrement);     //raise
+      printHighpassCutoff();  //print to USB Serial (ie, to the SerialMonitor)
+      updateFilterDisplay();  //update the App
+      break;
+    case 'T':
+      incrementHighpassFilters(1.0/freqIncrement); //lower
+      printHighpassCutoff();  //print to USB Serial (ie, to the SerialMonitor)
+      updateFilterDisplay();  //update the App
+      break;
+    case ']':
+      //this is the secret code from the App to START sending data for plotting
+      Serial.println("SerialManager: starting data flow for App SerialPlotter...");
+      myState.flag_printSignalLevels = true;
+      break;
+    case '}':
+      //this is the secret code from the App to STOP sending data for plotting
+      Serial.println("SerialManager: stopping data flow for App SerialPlotter...");
+      myState.flag_printSignalLevels = false;
+      break;
+    default:
+      //if the command didn't match any of the commands, it loops through the processCharacter() methods
+      //of any UI-enabled classes that were attached to the serialManager via the setupSerialManager() 
+      //function used back in the main *.ino file.
+      ret_val = SerialManagerBase::processCharacter(c);  //in here, it automatically loops over the different UI elements
+      break; 
+  }
+  return ret_val;
+}
+
+
+
+// //////////////////////////////////  Methods for defining the GUI and transmitting it to the App
+
+//define the GUI for the App
+void SerialManager::createTympanRemoteLayout(void) {
+  
+  // Create some temporary variables
+  TR_Page *page_h;  //dummy handle for a page
+  TR_Card *card_h;  //dummy handle for a card
+
+  //Add first page to GUI  (the indentation doesn't matter; it is only to help us see it better)
+  page_h = myGUI.addPage("TrebleBoost wComp");
+      //select inputs
+      card_h = earpieceMixer.addCard_audioSource(page_h); //use its predefined group of buttons for input audio source
+      
+      //Add card to this page for Highpass cutoff
+      card_h = page_h->addCard("Highpass Cutoff (Hz)");
+          card_h->addButton("-","T","",        4);  //displayed string, command, button ID, button width (out of 12)
+          card_h->addButton("", "", "cutoffHz",4);  //displayed string (blank for now), command (blank), button ID, button width (out of 12)
+          card_h->addButton("+","t","",        4);  //displayed string, command, button ID, button width (out of 12)
+
+  //add second page for more control of earpieces
+  page_h = earpieceMixer.addPage_digitalEarpieces(&myGUI); //use its predefined page for controlling the digital earpieces
+
+  //Add a page for the left compressor's built-in GUI
+  page_h = comp1.addPage_default(&myGUI);
+  page_h->setName("Left Compressor");  //I'm overwriting the default name so that we know that this is the left WDRC compressor
+
+  //Add a page for the right compressor's built-in GUI
+  page_h = comp2.addPage_default(&myGUI);
+  page_h->setName("Right Compressor");  //I'm overwriting the default name so that we know that this is the right WDRC compressor
+
+  //Add a page for some other miscellaneous stuff
+  page_h = myGUI.addPage("Globals");
+
+    //Add an example card that just displays a value...no interactive elements
+    //card_h = page_h->addCard(String("Analog Input Gain (dB)"));
+    //  card_h->addButton("", "", "inpGain", 12); //label, command, id, width (out of 12)...THIS IS FULL WIDTH!
+
+    //Add a button group ("card") for the CPU reporting...use a button group that is built into myState for you!
+    card_h = myState.addCard_cpuReporting(page_h);
+        
+  //add some pre-defined pages to the GUI (pages that are built-into the App)
+  myGUI.addPredefinedPage("serialPlotter");  //if we send data in the right format, the App will plot the signal levels in real-time!
+  myGUI.addPredefinedPage("serialMonitor");
+}
+
+
+// Print the layout for the Tympan Remote app, in a JSON-ish string
+void SerialManager::printTympanRemoteLayout(void) {
+    if (myGUI.get_nPages() < 1) createTympanRemoteLayout();  //create the GUI, if it hasn't already been created
+    String s = myGUI.asString();
+    Serial.println(s);
+    ble->sendMessage(s); //ble is held by SerialManagerBase
+    setFullGUIState();
+}
+
+// //////////////////////////////////  Methods for updating the display on the GUI
+
+void SerialManager::setFullGUIState(bool activeButtonsOnly) {  //the "activeButtonsOnly" isn't used here, so don't worry about it
+  
+  //First, let's update the portion of the GUI that we are explicitly defining and controlling ourselves
+  //here in the serial manager.
+  updateFilterDisplay();
+  //setButtonText("inpGain",String(myState.input_gain_dB,1));
+
+  //Then, let's have the system automatically update all of the individual UI elements that we attached
+  //to the serialManager via the setupSerialManager() function used back in the main *.ino file.
+  SerialManagerBase::setFullGUIState(activeButtonsOnly); //in here, it automatically loops over the different UI elements
+
+}
+
+void SerialManager::updateFilterDisplay(void) {
+  setButtonText("cutoffHz", String(myState.cutoff_Hz,0));
+}
+
+void SerialManager::sendDataToAppSerialPlotter(const String data1, const String data2) {
+  ble->sendMessage( String("P") + data1 + "," + data2 + String('\n') );
+}
+
+
+#endif
---
+++ examples/09-Earpieces/TrebleBoost_wComp_wEarpieces_wApp/TrebleBoost_wComp_wEarpieces_wApp.ino
@@ -0,0 +1,231 @@
+/*
+*   TrebleBoost_wComp_wApp
+*
+*   Created: Chip Audette, OpenAudio, August 2021
+*   Purpose: Process audio by applying a high-pass filter followed by a WDRC compressor.  
+*            Uses Tympan Earpieces.  Includes App interaction. 
+*
+*   TympanRemote App: https://play.google.com/store/apps/details?id=com.creare.tympanRemote
+*
+*   As a tutorial on how to interact with the mobile phone App, you should first explore the App Tutorial
+*   example "TrebleBoost_wComp_wApp", which showed how we were using the pre-written App GUI for changing
+*   all the settings in the compressor
+*
+*   Here in "TrebleBoost_wComp_wEarpieces_wApp", we started with the example sketch "TrebleBoost_wComp_wApp" 
+*   and added the earpiece functionality.  The earpieces also have built-in pre-written App GUI elements
+*   that we'll be using here.
+*
+*   Like any App-enabled sketch, this example shows you:
+*      * How to use Tympan code here to define a graphical interface (GUI) in the App
+*      * How to transmit that GUI to the App
+*      * How to respond to the commands coming back from the App
+*
+*   Compared to the previous example, this sketch:
+*      * Adds the earpieces and their GUI elements
+*
+*   MIT License.  use at your own risk.
+*/
+
+//here are the libraries that we need
+#include <Tympan_Library.h>  //include the Tympan Library
+
+//set the sample rate and block size
+const float sample_rate_Hz = 24000.0f ; //24000 or 44117 (or other frequencies in the table in AudioOutputI2S_F32)
+const int audio_block_samples = 32;     //do not make bigger than AUDIO_BLOCK_SAMPLES from AudioStream.h (which is 128)
+AudioSettings_F32 audio_settings(sample_rate_Hz, audio_block_samples);
+
+//create audio library objects for handling the audio
+Tympan                      myTympan(TympanRev::E, audio_settings);        //do TympanRev::D or TympanRev::E
+EarpieceShield              earpieceShield(TympanRev::E, AICShieldRev::A); //in the Tympan_Library, EarpieceShield is defined in AICShield.h
+
+//Create all the audio classes and make all of the audio connections
+#include      "AudioConnections.h"
+
+// Create classes for controlling the system
+#include      "SerialManager.h"
+#include      "State.h"                            
+BLE           ble(&Serial1);                       //create bluetooth BLE
+SerialManager serialManager(&ble);                 //create the serial manager for real-time control (via USB or App)
+State         myState(&audio_settings, &myTympan, &serialManager); //keeping one's state is useful for the App's GUI
+
+
+//define a function to configure the left and right compressors
+void setupMyCompressors(void) {
+  //set the speed of the compressor's response
+  float attack_msec = 5.0;
+  float release_msec = 300.0;
+  comp1.setAttack_msec(attack_msec); //left channel
+  comp2.setAttack_msec(attack_msec); //right channel
+  comp1.setRelease_msec(release_msec); //left channel
+  comp2.setRelease_msec(release_msec); //right channel
+
+  //Single point system calibration.  what is the SPL of a full scale input (including effect of input_gain_dB)?
+  float SPL_of_full_scale_input = 115.0;
+  comp1.setMaxdB(SPL_of_full_scale_input);  //left channel
+  comp2.setMaxdB(SPL_of_full_scale_input);  //right channel
+
+  //set the linear gain
+  float linear_gain_dB = 10.0;
+  comp1.setGain_dB(linear_gain_dB);  //set the gain of the Left-channel linear gain at start of compression
+  comp2.setGain_dB(linear_gain_dB);  //set the gain of the Right-channel linear gain at start of compression
+
+  //now define the compression parameters
+  float knee_compressor_dBSPL = 55.0;  //follows setMaxdB() above
+  float comp_ratio = 1.5;
+  comp1.setKneeCompressor_dBSPL(knee_compressor_dBSPL);  //left channel
+  comp2.setKneeCompressor_dBSPL(knee_compressor_dBSPL);  //right channel
+  comp1.setCompRatio(comp_ratio); //left channel
+  comp2.setCompRatio(comp_ratio); //right channel
+
+  //finally, the WDRC module includes a limiter at the end.  set its parameters
+  float knee_limiter_dBSPL = SPL_of_full_scale_input - 20.0;  //follows setMaxdB() above
+  //note: the WDRC limiter is hard-wired to a compression ratio of 10:1
+  comp1.setKneeLimiter_dBSPL(knee_limiter_dBSPL);  //left channel
+  comp2.setKneeLimiter_dBSPL(knee_limiter_dBSPL);  //right channel
+
+}
+
+//We're going to use a lot of built-in functionality of the compressor UI classes,
+//including their built-in encapsulation of state and settings.  Let's connect that
+//built-in stuff to our global State class via the function below.
+void connectClassesToOverallState(void) {
+  myState.comp1 = &comp1.state;
+  myState.comp2 = &comp2.state;
+  myState.earpieceMixer = &earpieceMixer.state;
+}
+
+//Again, we're going to use a lot of built-in functionality of the compressor UI classes,
+//including their built-in encapsulation of serial responses and App GUI.  Let's connect
+//that built-in stuff to our serial manager via the function below.  Once this is done,
+//most of the GUI stuff will happen in the background without additional effort!
+void setupSerialManager(void) {
+  //register all the UI elements here
+  serialManager.add_UI_element(&myState); //myState itself has some UI stuff we can use (like the CPU reporting!)
+  serialManager.add_UI_element(&earpieceMixer);
+  serialManager.add_UI_element(&comp1);   //The left compressor
+  serialManager.add_UI_element(&comp2);   //The right compressor
+}
+
+// define the setup() function, the function that is called once when the device is booting
+void setup() {
+  //begin the serial comms (for debugging)
+  myTympan.beginBothSerial(); delay(1000);
+  Serial.println("TrebleBoost_wComp_wEarpieces_wApp: Starting setup()...");
+
+  //allocate the dynamic memory for audio processing blocks
+  AudioMemory_F32(20,audio_settings); 
+
+  //Enable the Tympan to start the audio flowing!
+  myTympan.enable();       // activate the AIC on the main Tympan board
+  earpieceShield.enable(); // activate the AIC on the earpiece shield
+  earpieceMixer.setTympanAndShield(&myTympan, &earpieceShield); //the earpiece mixer must interact with the hardware, so point it to the hardware
+  
+  //setup the overall state and the serial manager
+  connectClassesToOverallState();
+  setupSerialManager();
+
+  //Choose the default input
+  if (1) {
+    //default to the digital PDM mics within the Tympan earpieces
+    earpieceMixer.setAnalogInputSource(EarpieceMixerState::INPUT_PCBMICS);  //Choose the desired audio analog input on the Typman...this will be overridden by the serviceMicDetect() in loop(), if micDetect is enabled
+    earpieceMixer.setInputAnalogVsPDM(EarpieceMixerState::INPUT_PDM);       // ****but*** then activate the PDM mics
+    Serial.println("setup: PDM Earpiece is the active input.");
+  } else {
+    //default to an analog input
+    earpieceMixer.setAnalogInputSource(EarpieceMixerState::INPUT_MICJACK_MIC);  //Choose the desired audio analog input on the Typman...this will be overridden by the serviceMicDetect() in loop(), if micDetect is enabled
+    Serial.println("setup: analog input is the active input.");
+  }
+  
+  //Set the Bluetooth audio to go straight to the headphone amp, not through the Tympan software
+  myTympan.mixBTAudioWithOutput(true);
+
+  //set volumes
+  myTympan.volume_dB(myState.output_gain_dB);  // -63.6 to +24 dB in 0.5dB steps.  uses signed 8-bit
+  myTympan.setInputGain_dB(myState.earpieceMixer->inputGain_dB); // set MICPGA volume, 0-47.5dB in 0.5dB setps
+
+  //set the highpass filter on the Tympan hardware to reduce DC drift
+  float hardware_cutoff_Hz = 40.0;  //set the default cutoff frequency for the highpass filter
+  myTympan.setHPFonADC(true, hardware_cutoff_Hz, audio_settings.sample_rate_Hz); //set to false to disable
+  earpieceShield.setHPFonADC(true, hardware_cutoff_Hz, audio_settings.sample_rate_Hz); //set to false to disable
+  
+  //setup BLE
+  while (Serial1.available()) Serial1.read(); //clear the incoming Serial1 (BT) buffer
+  ble.setupBLE(myTympan.getBTFirmwareRev());  //this uses the default firmware assumption. You can override!
+
+  //Set the cutoff frequency for the highpassfilter
+  setHighpassFilters_Hz(myState.cutoff_Hz);   //function defined near the bottom of this file
+  printHighpassCutoff();
+
+  //configure the left and right compressors with the desired settings
+  setupMyCompressors();
+
+  Serial.println("Setup complete.");
+  serialManager.printHelp();
+} //end setup()
+
+
+
+// define the loop() function, the function that is repeated over and over for the life of the device
+void loop() {
+
+  //look for in-coming serial messages (via USB or via Bluetooth)
+  if (Serial.available()) serialManager.respondToByte((char)Serial.read());   //USB Serial
+
+  //respond to BLE
+  if (ble.available() > 0) {
+    String msgFromBle; int msgLen = ble.recvBLE(&msgFromBle);
+    for (int i=0; i < msgLen; i++) serialManager.respondToByte(msgFromBle[i]);
+  }
+
+  //service the BLE advertising state
+  ble.updateAdvertising(millis(),5000); //check every 5000 msec to ensure it is advertising (if not connected)
+
+  //periodically print the CPU and Memory Usage
+  if (myState.flag_printCPUandMemory) myState.printCPUandMemory(millis(), 3000); //print every 3000msec  (method is built into TympanStateBase.h, which myState inherits from)
+  if (myState.flag_printCPUandMemory) myState.printCPUtoGUI(millis(), 3000);     //send to App every 3000msec (method is built into TympanStateBase.h, which myState inherits from)
+
+  //periodically print the gain status
+  if (myState.flag_printSignalLevels) printSignalLevels(millis(),1000); //update every 1000 msec
+
+} //end loop();
+
+
+// ///////////////// Servicing routines
+
+//This routine plots the input and output level of the left compressor
+void printSignalLevels(unsigned long curTime_millis, unsigned long updatePeriod_millis) {
+  static unsigned long lastUpdate_millis = 0;
+  
+  //has enough time passed to update everything?
+  if (curTime_millis < lastUpdate_millis) lastUpdate_millis = 0; //handle wrap-around of the clock
+  if ((curTime_millis - lastUpdate_millis) > updatePeriod_millis) { //is it time to update the user interface?
+    
+    float in_dB = comp1.getCurrentLevel_dB();         //here is what the compressor is seeing at its input
+    float gain_dB = comp1.getCurrentGain_dB();
+    float out_dB = in_dB + gain_dB; //this is the ouptut, including the effect of the changing gain of the compressor
+    
+    myTympan.println("State: printLevels: Left In, gain, Out (dBFS) = " + String(in_dB,1) + ", " + String(gain_dB,1) + ", " + String(out_dB,1)); //send to USB Serial
+    serialManager.sendDataToAppSerialPlotter(String(in_dB,1),String(out_dB,1));  //send to App (for plotting)
+  
+    lastUpdate_millis = curTime_millis;
+  } // end if
+}
+
+
+
+// ///////////////// functions used to respond to the commands
+
+//change the highpass cutoff frequency
+const float min_allowed_Hz = 100.0f;  //minimum allowed cutoff frequency (Hz)
+const float max_allowed_Hz = 0.9 * (sample_rate_Hz/2.0);  //maximum allowed cutoff frequency (Hz);
+void incrementHighpassFilters(float scaleFactor) { setHighpassFilters_Hz(myState.cutoff_Hz*scaleFactor); }
+float setHighpassFilters_Hz(float freq_Hz) {
+  myState.cutoff_Hz = min(max(freq_Hz,min_allowed_Hz),max_allowed_Hz); //set the new value as the new state
+  hp_filt1.setHighpass(0, myState.cutoff_Hz); //biquad IIR filter.  left channel
+  hp_filt2.setHighpass(0, myState.cutoff_Hz); //biquad IIR filter.  right channel
+  return myState.cutoff_Hz;
+}
+
+void printHighpassCutoff(void) {
+  Serial.println("Highpass filter cutoff at " + String(myState.cutoff_Hz,0) + " Hz");
+}
