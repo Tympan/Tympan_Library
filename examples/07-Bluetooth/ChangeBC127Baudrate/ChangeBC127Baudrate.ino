65222d0e84f0fb545c87b319dbb19b61b597d6a0
Chip Audette <chipaudette@yahoo.com>
2021-08-25 18:09:55 -0400

    ChangeBC127Baudrate: start with hardware reset, and interactive baudrate changing ability

--- examples/07-Bluetooth/ChangeBC127Baudrate/ChangeBC127Baudrate.ino
+++ examples/07-Bluetooth/ChangeBC127Baudrate/ChangeBC127Baudrate.ino
@@ -20,122 +20,104 @@
 
 #include <Arduino.h>
 #include <Tympan_Library.h>
-BLE        ble(&Serial1);
 
 // USER PARAMETERS
 const int TARGET_BAUDRATE = 115200;    //try 115200 (fast!) or 9600 (factory original)
-const int FIRMWARE_VERSION = 7;        // what Melody firmware we are dealing with
-const bool USE_HARDWARE_RESET = true;  //try to use the BC127 GPIO Pins to force a factory reset
 
-const int pin_PIO0 = 5; //RevD = 56, RevE = 5   // Pin # for connection to BC127 PIO0 pin
-const int pin_RST = 9;  //RevD = 34, RevE = 9   // Pin # for connection to BC127 RST pin
+// Automatically choose which Tympan and which firmware (you can override!)
+#ifdef KINETISK  //this is set by the Arduino IDE when you choose Teensy 3.6
+  //REV D...does not work reliably with RevD.  I don't know why
+  Tympan myTympan(TympanRev::D);      //sets all the control pins correctly
+  const int FIRMWARE_VERSION = 5;     // what Melody firmware we are dealing with
+#else
+  //REV E
+  Tympan myTympan(TympanRev::E);       //sets all the control pins correctly
+  const int FIRMWARE_VERSION = 7;      // what Melody firmware we are dealing with
+#endif
 
+//define the pins that are used for the hardware reset
+const int pin_PIO0 = myTympan.getTympanPins().BT_PIO0;   //RevD = 56, RevE = 5   // Pin # for connection to BC127 PIO0 pin
+const int pin_RST = myTympan.getTympanPins().BT_nReset;  //RevD = 34, RevE = 9   // Pin # for connection to BC127 RST pin
+const int pin_VREGEN = myTympan.getTympanPins().BT_REGEN; 
+
+const int baudrate_factory = 9600;       // here is the factory default
 
 // //////////////////////////////////////////// Helper functions
 
-const int baudrate_factory = 9600;       // here is the factory default
-const int baudrate1 = baudrate_factory;  //here is one possible starting baudrate for the BLE module
-const int baudrate2 = 115200;            //here is the other possible starting baudrate for the BLE module
-int cur_baudrate;  // this is the baudrate that we've set Serial1 to be
-void setSerial1BaudRate(int new_baud) {
-  Serial1.flush();
-  Serial1.end();
-  delay(500);
-  Serial1.begin(new_baud);
-  delay(500);
-  cur_baudrate = new_baud;
-}
 
+void setSerial1BaudRate(int new_baud) {
+  Serial1.flush();  Serial1.end();   Serial1.begin(new_baud);
+}
 
 //This function attempts to hold PIO 0 high and then reseting via the reset pin.
 //See Melody Guide for v7 "Restoring the Default Configuration".
 void hardwareReset() {
-  //Set the reset pin.  "Normal" is high impedance with an external pull-up.  Let's
-  //set our default to high-impedance ("INPUT") with also an internal pullup, then
-  //transition to low-impedance (ie, "OUTPUT") set high
-  pinMode(pin_RST, INPUT);
-  digitalWrite(pin_RST, HIGH);
-  pinMode(pin_RST, OUTPUT);
-  digitalWrite(pin_RST, HIGH);
   
-  //now set the PIO0 pin.  Normal is PIO0
-  pinMode(pin_PIO0,OUTPUT);
-  digitalWrite(pin_PIO0, LOW);
-
-  //stall for a tiny bit (for no good reason)
-  delay(50);
-
-  //Now we follow the instructions in the Melody User Guide
-  //
-  // The default configuration can be restored using either of the following equivalent 
-  // methods:
-  //   * Maintain PIO 0 high (press the VOL UP button on a BC127-DISKIT) while 
-  //     resetting the module
-  //   * Use RESTORE
-  digitalWrite(pin_PIO0,HIGH);
-  delay(5); //let it setting
+  //Serial.println("HardwareReset: RST Pin = " + String(pin_RST) + ", PIO0 Pin = " + String(pin_PIO0));
   
-  //And, from the Datasheet, reset if low for more than 5msec
-  digitalWrite(pin_RST,LOW);  //pull the pin low to start reset timer
-  delay(10);                  //resets if low for more than 5 msec
-  digitalWrite(pin_RST,LOW);  //return to normal
+  pinMode(pin_PIO0,OUTPUT);    //prepare the PIO0 pin
+  digitalWrite(pin_PIO0,HIGH); //normally low.  Swtich high
 
-  //did it reset?  It should report its ID info to the Serial
+  pinMode(pin_RST,OUTPUT);    //prepare the reset pin
+  digitalWrite(pin_RST,LOW);  //pull low to reset
+  delay(20);                  //hold low for at least 5 msec
+  digitalWrite(pin_RST,HIGH); //pull high to start the boot
 
-  //return to normal
-  delay(10);
-  digitalWrite(pin_PIO0,LOW); //return to normal
+  //wait for boot to proceed far enough before changing anything
+  delay(400);                 //V7: works with 200 but not 175.  V5: works with 350 but not 300
+
+  digitalWrite(pin_PIO0,LOW); //pull PIO0 back down low
+  pinMode(pin_PIO0,INPUT);    //go high-impedance to make irrelevant
+
 }
 
-int hardwareResetWithChecking(void) {
-  Serial.println("setup(): Attempting a hardware reset...");
-    
-  //if successfull, we'll end up in the factory baudrate.  So, let's switch
-  //so that we can get its boot-up message
-  setSerial1BaudRate(baudrate_factory);
-
-  //do the hardware reset
-  hardwareReset();
-
-  //check for the boot message
-  delay(500);
-  echoIncomingBTSerial();
-  Serial.println("Did you see the boot-up info from the BC127??");
-
-  //check BT status (another way to confirm that we have the right speed
-  ret_val = checkStatusBLE();
-  if (ret_val != BC127::SUCCESS) {
-    Serial.println("setup(): Hardware reset FAILED to restore factory baudrate.");
-    return -1; //fail
-  } else {
-    Serial.println("setup(): Hardware reset SUCCESSS in restoring factory baudrate.");
-  } 
-  return 0; //assume success 
-}
-
-//If the baudrate to the BLE modue matches that baudrate that the BLE module expects,
-//we should be able to ask its status and get a valid reply.  So, we don't get a valid
-//reply, the baudrate of Serial1 probably does not match.
-int checkStatusBLE(void) {
-  int ret_val = ble.status(true);
-  Serial.println("checkStatusBLE: reply code to status = " + String(ret_val));
-  if (ret_val != BC127::SUCCESS) {
-    Serial.println("checkStatusBLE: status() returned error: " + String(ret_val));
+void changeBaudRateBC127(int target_rate, int firmware_ver) {
+  //Serial.println("setup: Firmware V" + String(firmware_ver) + ": Change baudrate on the BC127 to "  + String(target_rate) + "...");
+  if (firmware_ver > 5)  {  
+    //On version 7.3, the baud rate shift is instant.
+    Serial1.print("SET UART_CONFIG=");Serial1.print(target_rate);Serial1.print(" OFF 0");Serial1.print('\r');
+  } else  {
+    //On version 5, the baud rate shift is instant.
+    Serial1.print("SET BAUD=");Serial1.print(target_rate);Serial1.print('\r');
   }
-  return ret_val;
 }
 
 // Echo what's coming in on USB Serial out to Bluetooth Serial
 void echoIncomingUSBSerial(void) {
-  while (Serial.available()) Serial1.write(Serial.read());
+    while (Serial.available())
+    {
+      char c = Serial.read();
+      if (c == '|') {
+          Serial.println("Switching Serial1 to 115200...");
+          setSerial1BaudRate(115200);
+      } else if (c =='\\') { 
+          Serial.println("Switching Serial1 to 9600...");
+          setSerial1BaudRate(9600);
+      } else if (c == ']') {
+          Serial.println("Hardware reset...");
+          hardwareReset();
+      } else {
+          Serial1.write(c); //send to Bluetooth
+      }
+    }
 }
 
 // Echo what's coming in on Bluetooth Serial out to USB Serial
 void echoIncomingBTSerial(void) {
+  static char prev_char;
   while (Serial1.available()) {
-    char c = Serial1.read();
-    if (c == '\r') c = '\n'; //translate carriage return to newline
-    Serial.write(c);
+    char orig_c = Serial1.read();
+    char c = orig_c;
+    if ((c == '\n') && (prev_char == '\r')) {
+      //don't send
+    } else if ((c == '\r') && (prev_char == '\n')) {
+      //don't send
+    } else {
+      //send the character (perhaps modified)
+      if (c == '\r') c = '\n'; //translate carriage return to newline
+      Serial.write(c);
+    }
+    prev_char = orig_c;
   }
 }
 
@@ -143,61 +125,70 @@ void echoIncomingBTSerial(void) {
 // ///////////////////////////////////////////////////  Arduino setup() and loop()
 
 void setup() {
-  //Serial.begin(115200);  //on Teensy (and Tympan), you don't need to do this.
 
-  //tell the BLE class what firmware we think that is actually on the BC127
-  ble.set_BC127_firmware_ver(FIRMWARE_VERSION);
+  //////////////////////////////////////////////// Factory reset to known baud rate (9600)
 
-  //Start Serial1 to the BT module
-  Serial.println("setup(): Starting Serial1 at " + String(baudrate1));
-  setSerial1BaudRate(baudrate1);
+  Serial.println("setup(): Setting Teensy Serial1 at " + String(baudrate_factory));
+  setSerial1BaudRate(baudrate_factory);
 
-  //check on ble module...
-  int ret_val = checkStatusBLE();
-  if (ret_val != BC127::SUCCESS) {
+  //clear out buffers
+  //while (Serial.available()) Serial.read();
+  //while (Serial1.available()) Serial1.read();
 
-    //try a different baud rate
-    Serial.println("setup(): Trying again but at " + String(baudrate2));
-    setSerial1BaudRate(baudrate2);
-    cur_baudrate = baudrate2;
+  //Force a hardware reset...which should drop baudrate to factory setting
+  Serial.println("setup(): hardware reset...");
+  hardwareReset();  
+  delay(2000);            //delay of 1250 seems to work for V5 but 1000 occasionally fails.  1250 failed once.
+  echoIncomingBTSerial(); //should get Melody ID text (end with READY or OK)
+  Serial.println();
 
-    //check on ble module again...
-    ret_val = checkStatusBLE();
-    if (ret_val == BC127::SUCCESS) {
+  ///////////////////////////////////////////////// Change baud rate on the BC127
 
-      // we were successful at the new baud rate, so stick with it!
-      Serial.println("Success! The module was at " + String(baudrate2));
+  //Switch the baudrate on the BC127
+  Serial.print("setup(): commanding BC127 to change baud rate to "); Serial.println(TARGET_BAUDRATE);
+  changeBaudRateBC127(TARGET_BAUDRATE, FIRMWARE_VERSION);
+ 
+  //Switch Teensy serial to the matching speed
+  setSerial1BaudRate(TARGET_BAUDRATE);
+  Serial.println("setup(): switching Teensy Serial1 to new baud rate " + String(TARGET_BAUDRATE));
 
-    } else {
-      //Failed.  So, return Serial1 to original baud rate
-      Serial.println("Returning to Serial1 to original baud rate...this whole thing probably will not work");
-      setSerial1BaudRate(baudrate1);
-      cur_baudrate = baudrate1;
-    }
+  //Listen for response to the change in baud rate (or, at least, clear out the serial
+  delay(500);              //500 seems to work on V5
+  echoIncomingBTSerial();  //clear out the serial
+
+  //send carriage return to induce "ERROR" message to clear out the serial link both ways
+  Serial.println("setup(): issuing Carriage Return to clear serial...should return ERROR...");
+  Serial1.print('\r');  //should cause response of "ERROR"
+  delay(100);  echoIncomingBTSerial();  //should get error
+
+  //send WRITE to save the baud rate and send another writes just to clear up the Serial link
+  Serial.println("setup(): issuing WRITE command to save the new rate");
+  Serial1.print("WRITE");Serial1.print('\r');
+  delay(200);  echoIncomingBTSerial();  //should get OK
+  
+  //Serial.println("setup(): issuing second WRITE command to save the new rate to ensure we're good");
+  //Serial1.print("WRITE");Serial1.print('\r');
+  //delay(200);  echoIncomingBTSerial();  //should get OK
+  
+  ///////////////////////////////////////////////// Check to make sure that we're good
+
+  Serial.println("setup(): Asking baud rate of BC127...");
+  if (FIRMWARE_VERSION > 6) {
+    Serial1.print("GET UART_CONFIG");Serial1.print('\r');
+  } else {
+    Serial1.print("GET BAUD");Serial1.print('\r');
   }
+  delay(200); echoIncomingBTSerial();  //should give the value of the new baud rate
 
-  //try forcing a hardware reset
-  if (USE_HARDWARE_RESET) hardwareResetWithChecking();
-
-  //switch to desired baudrate
-  if (cur_baudrate != TARGET_BAUDRATE) {
-    //let's try commanding the BC127 to the new baud rate anyway
-    Serial.println("Change baudrate on the BC127 to "  + String(TARGET_BAUDRATE) + "...");
-    char cmdBuffer[100];
-    if (FIRMWARE_VERSION > 5)  {
-      sprintf(cmdBuffer, "SET UART_CONFIG=%u OFF 0\rWRITE\rRESTORE\r", TARGET_BAUDRATE);  //restore? or do we want reset?
-    } else  {
-      sprintf(cmdBuffer, "SET BAUD=%u\rWRITE\rRESTORE\r", TARGET_BAUDRATE);  //restore?  or do we want reset?
-    }
-    Serial1.write(cmdBuffer);  delay(500);
-    echoIncomingBTSerial();
-
-    Serial.println("Switching Serial1 to new baud rate..." + String(TARGET_BAUDRATE));
-    setSerial1BaudRate(TARGET_BAUDRATE);
-    cur_baudrate = TARGET_BAUDRATE;
-  }
-
-  Serial.println("Begin echoing commands with BC127.  Try BC127 commands like STATUS or VERSION...(carriage return!)");
+  Serial.println();
+  Serial.println("We are done!  Now echoing commands with the BC127...");
+  Serial.println();
+  Serial.println("To change BC127 baud rate: for V5, send: SET BAUD=115200   for V7: send: SET UART_CONFIG=115200 OFF 0");
+  Serial.println("To change Tympan baud rate, send '|' for 115200 or send '\\' for 9600");
+  Serial.println("To force a hardware reset of the BC127, send ']'");
+  Serial.println();
+  Serial.println("Try BC127 commands like STATUS or VERSION or ADVERTISING ON...(with carriage return!)");
+  Serial.println();  
 }
 
 void loop() {
