af78242146be34089f39624b3ff42d9ea660df99
Chip Audette <chipaudette@yahoo.com>
2023-08-25 12:26:59 -0400

    AudioPlayMemory: add ability to queue multiple samples

--- src/AudioPlayMemory.cpp
+++ src/AudioPlayMemory.cpp
@@ -2,16 +2,43 @@
 #include "AudioPlayMemory_F32.h"
 
 
-bool AudioPlayMemoryI16_F32::play(const int16_t *_data_ptr, const uint32_t _data_len) {
-  state = STOPPED;
-  if ((_data_ptr == NULL) || (_data_len == 0)) return false; //nothing to play, so return
-  data_ptr = _data_ptr;
-  data_len = _data_len;
-  data_ind = 0;  data_phase = 0.0f; //reset data index incrementing
-  state = PLAYING;                  //start playing
-  return isPlaying();
-} 
+int AudioPlayMemoryQueue::addSample(SampleInfo sample) {
+	if (queue_len >= MAX_AUDIOPLAYMEMORYQUEUE_LEN) {
+		Serial.println("AudioPlayMemoryQueue: addSample: *** ERROR ***: Queue already has " + String(queue_len) + " samples");
+		Serial.println("                               : Cannot add another sample.");
+		return -1;
+	} else {
+		int cur_write_ind = queue_len;
+		all_sampleInfo[cur_write_ind] = sample; //I want this to make a local copy.  Hopefully it does!
+		queue_len++;  //queue is now one longer
+	}
+	Serial.println("AudioPlayMemoryQueue:addSample: queue length = " + String(queue_len)); 
+	return queue_len;
+}
 
+int AudioPlayMemoryI16_F32::setCurrentSampleFromQueue(int ind) {
+	if (ind < 0) return -1;
+	const int queueLen = playQueue.getQueueLen();
+	if (queueLen == 0) return -1;
+	if (ind >= queueLen) return -1;
+	
+	//copy the sample information
+	queue_ind = ind;
+	data_ptr = playQueue.all_sampleInfo[queue_ind].data_ptr;
+	data_len = playQueue.all_sampleInfo[queue_ind].length;
+	setDataSampleRate_Hz(playQueue.all_sampleInfo[queue_ind].sample_rate_Hz);
+
+	//reset the playback counters
+	data_ind = 0;
+	data_phase = 0.0f;
+	
+	Serial.println("AudioPlayMemoryI16_F32: setCurrentSampleFromQueue: queue ind = " + String(queue_ind)
+	    + ", data_ptr = " + String((int)data_ptr) 
+		+ ", data_len = " + String(data_len)
+		+ ", sample_rate = " + String(sample_rate_Hz));
+	return queue_ind;
+}
+		
 #define CONVERT_I16_TO_F32(x) (((float32_t)x)/(16384.0f))
 
 void AudioPlayMemoryI16_F32::update(void) {
@@ -21,29 +48,52 @@ void AudioPlayMemoryI16_F32::update(void) {
   audio_block_f32_t *out_block = AudioStream_F32::allocate_f32();
   if (out_block == NULL) return; //no memory available.
 
-  //fill the memory
+  //fill the out_block with audio
   for (uint32_t dest_ind = 0; dest_ind < ((uint32_t)out_block->length); dest_ind++) {
-
-    //Serial.println("AudioPlayMemoryI16_F32: update: data_phase = " + String(data_phase) + ", data_ind = " + String(data_ind));
-    
-    if (data_ind < data_len) { //is there more audio to read in the memory buffer
-      out_block->data[dest_ind] = CONVERT_I16_TO_F32(data_ptr[data_ind]); 
-
-      //compute index for next time
-      data_phase += data_phase_incr;                  //increment the phase
-      uint32_t whole_step = (unsigned int)data_phase; //have we increased a whole step yet?
-      data_ind += whole_step;                         //increment the data index by any whole steps
-      data_phase -= (float32_t)whole_step;            //remove the whole step from the data_phase to keep it well bounded
-
-    } else { //no, there is not more audio to read in the memory buffer
-      
-      state = STOPPED;
-      out_block->data[dest_ind] = 0.0;
-      
-    }
+	 out_block->data[dest_ind] = getNextAudioValue(); //in this method, it will also alter the play/stop state
   }
 
   //transmit and release
   AudioStream_F32::transmit(out_block);
   AudioStream_F32::release(out_block);
 }
+
+float32_t AudioPlayMemoryI16_F32::getNextAudioValue(void) {
+	//Serial.println("AudioPlayMemoryI16_F32: update: data_phase = " + String(data_phase) + ", data_ind = " + String(data_ind));
+	
+	
+	if (data_ind >= data_len) { //have we reached the end of the current data buffer?
+		//yes, we've reached the end of the current buffer. 
+      
+		//is there another buffer of audio in the queue?
+		int candidate_queue_ind = queue_ind + 1;
+		if (candidate_queue_ind >= playQueue.getQueueLen()) {
+			//there are no more queue'd data buffers, so we're done
+			state = STOPPED;
+			return 0.0;  //give a zero value return
+		} else {
+			//there are more buffers in the queue, so let's use them!
+			setCurrentSampleFromQueue(candidate_queue_ind);  //switch to the new audio buffer
+		}
+	}
+	
+	//catch a degenerate case: if the current data buffer has zero length.  In this weird case
+	//let's just issue a zero and return, without changing the play state.  The next time
+	//this function gets called, it'll try to increment the buffers, or it'll stop playing.
+	//It would have been great to do that incrementing now, but I don't have time.  Sorry.
+	if (data_ind >= data_len) return 0.0;
+	
+	//if we're here, we should have data to work with.  So, let's go!
+	
+	// get the current data value from the buffer
+	float ret_val = CONVERT_I16_TO_F32(data_ptr[data_ind]);   //get the next piece of data
+
+	//compute the data index for next time this gets called
+	data_phase += data_phase_incr;                  //increment the phase
+	uint32_t whole_step = (unsigned int)data_phase; //have we increased a whole step yet?
+	data_ind += whole_step;                         //increment the data index by any whole steps
+	data_phase -= (float32_t)whole_step;            //remove the whole step from the data_phase to keep it well bounded
+	
+	return ret_val;
+  }
+	
--- src/AudioPlayMemory_F32.h
+++ src/AudioPlayMemory_F32.h
@@ -6,7 +6,6 @@
 #include <AudioSettings_F32.h>
 #include <AudioStream_F32.h>
 
-
 /*
  * AudioPlayMemory16_F32
  * 
@@ -18,7 +17,60 @@
  *        But, as of writing this comment, it does not interpolate or filter; it only does 
  *        a very-simple zero-order-hold (ie, stair-step) based on the closest sample.  You
  *        might not like the audio artifacts that result.   Feel free to improve it!
+ *
+ * Notes: This player will play a single sample or it can seemlessly play a series of samples.
+ *            - If you want a single sample, you can specify just that one sample as a data array
+ *              plus its length and its sample rate.  Or, you can wrap up that same info in a
+ *              "SampleInfo" object.
+ *            - If you want to play a series of samples in a row, you must use an "AudioPlayMemoryQueue"
+ *              object.  This object holds a bunch of "SampleInfo" instances within it.
  */
+ 
+ //define a structure to hold the sample info...but NOT the sample values themselves, just the info
+class SampleInfo {
+	public:
+		SampleInfo() {};
+		SampleInfo(const int16_t *_data, const uint32_t _data_len, const float32_t _data_fs_Hz) {
+			data_ptr = _data; length = _data_len; sample_rate_Hz = _data_fs_Hz;
+		}
+		SampleInfo& operator=(const SampleInfo& sample) { //ensure a proper (shallow) copy
+			data_ptr = sample.data_ptr;
+			length = sample.length;
+			sample_rate_Hz = sample.sample_rate_Hz;
+			return *this;
+		}
+		const int16_t *data_ptr;
+		uint32_t length;
+		float32_t sample_rate_Hz;
+};
+
+//if you want to queue up many samples, use the class below!
+#define MAX_AUDIOPLAYMEMORYQUEUE_LEN 10
+class AudioPlayMemoryQueue {
+	public:
+		AudioPlayMemoryQueue(void) { reset(); };
+		AudioPlayMemoryQueue(const int16_t *data, const uint32_t data_len, const float32_t data_fs_Hz) {
+			reset(); 
+			addSample(data,data_len,data_fs_Hz);
+		}
+		AudioPlayMemoryQueue(SampleInfo sample) { addSample(sample); } 
+		
+		int addSample(const int16_t *data, const uint32_t data_len, const float32_t data_fs_Hz) {
+			return addSample(SampleInfo(data,data_len,data_fs_Hz));
+		}
+		int addSample(SampleInfo sample);
+		int getQueueLen(void) { return queue_len; };
+		void reset(void) { queue_len = 0; }
+		AudioPlayMemoryQueue& operator=(const AudioPlayMemoryQueue& queue) { //ensure a proper copy
+			queue_len = queue.queue_len;
+			for (int i=0; i<MAX_AUDIOPLAYMEMORYQUEUE_LEN; i++) all_sampleInfo[i] = queue.all_sampleInfo[i];
+			return *this;
+		}
+		SampleInfo all_sampleInfo[MAX_AUDIOPLAYMEMORYQUEUE_LEN];
+	private:
+		int queue_len = 0;
+};
+ 
 class AudioPlayMemoryI16_F32 : public AudioStream_F32
 {
 //GUI: inputs:0, outputs:1  //this line used for automatic generation of GUI nodes 
@@ -26,16 +78,34 @@ class AudioPlayMemoryI16_F32 : public AudioStream_F32
     AudioPlayMemoryI16_F32(void) : AudioStream_F32(0, NULL) { }
     AudioPlayMemoryI16_F32(const AudioSettings_F32 &settings) : AudioStream_F32(0, NULL)
     { 
-      setSampleRate_Hz(settings.sample_rate_Hz);  setDataSampleRate_Hz(settings.sample_rate_Hz); 
+      setSampleRate_Hz(settings.sample_rate_Hz);  
+	  setDataSampleRate_Hz(settings.sample_rate_Hz); 
     }
-    virtual bool play(const int16_t *data, const uint32_t data_len);
-    virtual bool play(const int16_t *data, const uint32_t data_len, const float32_t data_fs_Hz) {
-      setDataSampleRate_Hz(data_fs_Hz);
-      return play(data, data_len);
+	
+	// here are the play methods.  Here's where you tell it to start playing (and what to play)
+	virtual bool play(AudioPlayMemoryQueue _playQueue) { 
+		stop();	                       //stop any playback that might be already happening
+		playQueue = _playQueue;        //make a local copy of the new queue
+		setCurrentSampleFromQueue(0);  //start at queue index zero
+		state = PLAYING;               //set the flag to allow the update() method to start playing
+		return state;
+	}
+    virtual bool play(SampleInfo &sample) { 
+		return play(AudioPlayMemoryQueue(sample));  //wrap the sample info into a queue
+	}
+	virtual bool play(const int16_t *data, const uint32_t data_len, const float32_t data_fs_Hz) {
+		return play(AudioPlayMemoryQueue(SampleInfo(data,data_len,data_fs_Hz)));  //wrap the sample info into a queue
     }
-    virtual void stop(void) { state = STOPPED; }
+	
+	//here's how you check in on whether it is playing
+	enum STATE {STOPPED=0, PLAYING};
     virtual bool isPlaying(void) { if (state == PLAYING) { return true; } else { return false; } }
-    virtual void update(void);
+
+	//here's how you force it to stop playing
+    virtual void stop(void) { state = STOPPED; }	
+	
+	//utility stuff
+    virtual void update(void); //here is what plays out the audio to the audio subsystem
     float setSampleRate_Hz(float fs_Hz) { 
       sample_rate_Hz = fs_Hz; 
       data_phase_incr = data_sample_rate_Hz / sample_rate_Hz;
@@ -47,8 +117,9 @@ class AudioPlayMemoryI16_F32 : public AudioStream_F32
       //Serial.println("AudioPlayMemoryI16_F32: data_fs_Hz = " + String(data_fs_Hz) + ", data_phase_incr = " + String(data_phase_incr));
       return data_sample_rate_Hz;
     }
+	int setCurrentSampleFromQueue(int ind);
 
-    enum STATE {STOPPED=0, PLAYING};
+
     private:
       int state = STOPPED;
       const int16_t *data_ptr = NULL;
@@ -59,6 +130,11 @@ class AudioPlayMemoryI16_F32 : public AudioStream_F32
       float32_t data_sample_rate_Hz = ((float)AUDIO_SAMPLE_RATE_EXACT);
       float sample_rate_Hz = ((float)AUDIO_SAMPLE_RATE_EXACT);
       //int audio_block_samples = AUDIO_BLOCK_SAMPLES;
+	  
+	  //related to the queue of audio samples
+	  AudioPlayMemoryQueue playQueue;
+	  float32_t getNextAudioValue(void);
+	  int queue_ind;
 };
 
 
