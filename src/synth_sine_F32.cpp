00ab89a99e13f16772c1f3ca4180f201b7624c08
Chip Audette <chipaudette@yahoo.com>
2024-05-27 18:37:49 -0400

    synth_sine_F32: add _UI elements for automatic App GUI

--- src/synth_sine_F32.cpp
+++ src/synth_sine_F32.cpp
@@ -14,11 +14,37 @@
 #include "utility/dspinst.h"
 #include "utility/data_waveforms.c"
 
+
+// ///////////////////////////////////////////////////////////////////////////////////
+// //////////////////////////// Define the methods for the AudioSynthWaveformSineState
+// ///////////////////////////////////////////////////////////////////////////////////
+
+float AudioSynthWaveformSineState::getSampleRate_Hz(void) { if (synthWaveform) { return synthWaveform->getSampleRate_Hz(); } else { return -1.0; } };
+float AudioSynthWaveformSineState::getFrequency_Hz(void) { if (synthWaveform) { return synthWaveform->getFrequency_Hz(); } else { return -1.0; } };
+float AudioSynthWaveformSineState::getAmplitude(void) { if (synthWaveform) { return synthWaveform->getAmplitude(); } else { return -1.0; } };
+float AudioSynthWaveformSineState::getAmplitude_dBFS(void) { if (synthWaveform) { return synthWaveform->getAmplitude_dBFS(); } else { return -1.0; } };
+void AudioSynthWaveformSineState::setSynthWaveform(AudioSynthWaveformSine_F32 *c) { synthWaveform = c; }
+void AudioSynthWaveformSineState::printWaveformParameters(void) { 
+	Serial.println("Waveform Parameters:");
+	Serial.println("  Sample rate (Hz) = " + String(getSampleRate_Hz(),0));
+	Serial.println("  Frequency (Hz) = " + String(getFrequency_Hz(),1));
+	Serial.println("  Amplitude (dB Peak re: FS) = " + String(getAmplitude_dBFS(),1));
+}
+
+// ///////////////////////////////////////////////////////////////////////////////////
+// //////////////////////////// Define the methods for the AudioSynthWaveformSine_F32
+// ///////////////////////////////////////////////////////////////////////////////////
+
 // data_waveforms.c
 //extern "C" {
 //  extern const int16_t AudioWaveformSine[257];
 //}
 
+void AudioSynthWaveformSine_F32::setDefaultValues(void) {
+	state.setSynthWaveform(this);
+	setFrequency_Hz(1000.0f);
+	setAmplitude_dBFS(-12.0f);
+}
 
 void AudioSynthWaveformSine_F32::update(void)
 {
@@ -65,7 +91,133 @@ void AudioSynthWaveformSine_F32::update(void)
 }
 
 
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// AudioSynthWaveformSine_F32 UI Methods
+//
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void AudioSynthWaveformSine_F32_UI::printHelp(void) {
+	String prefix = getPrefix();  //getPrefix() is in SerialManager_UI.h, unless it is over-ridden in this class somewhere
+	Serial.println(" " + name_for_UI + ": Prefix = " + prefix);
+	Serial.println(F("   e,E: Enable/disable tone (currently ") + String(getEnabled() ? "Enabled" : "Disabled") + ")");
+	Serial.println(F("   f,F: Incr/decrease frequency (") + String(getFrequency_Hz(),1) + " Hz)");
+	Serial.println(F("   a,A: Incr/decrease amplitude (") + String(getAmplitude_dBFS(),1) + " dBFS)"); 
+
+}; 
+
+
+bool AudioSynthWaveformSine_F32_UI::processCharacterTriple(char mode_char, char chan_char, char data_char) {
+	
+	//check the mode_char to see if it corresponds with this instance of this class.  If not, return with no action.
+	if (mode_char != ID_char) return false;  //ID_char is from SerialManager_UI.h
+
+	//we ignore the chan_char and only work with the data_char
+	bool return_val = true;  //assume that we will find this character
+	switch (data_char) {
+		case 'e':
+			setEnabled(true);
+			Serial.println(F("AudioSynthWaveformSine_F32_UI: eanble tone = ") + String(getEnabled()));
+			updateCard_enable();
+			break;
+		case 'E':
+			setEnabled(false);
+			Serial.println(F("AudioSynthWaveformSine_F32_UI: eanble tone = ") + String(getEnabled()));
+			updateCard_enable();
+			break;
+		case 'a':
+			incrementAmplitude_dBFS(amp_fac_dB);
+			Serial.println(F("AudioSynthWaveformSine_F32_UI: changed amplitude to ") + String(getAmplitude_dBFS(),1) + " dBFS"); 
+			updateCard_amp_dBFS();  //send updated value to the GUI
+			break;
+		case 'A':
+			incrementAmplitude_dBFS(-amp_fac_dB);
+			Serial.println(F("AudioSynthWaveformSine_F32_UI: changed amplitude to ") + String(getAmplitude_dBFS(),1) + " dBFS"); 
+			updateCard_amp_dBFS();  //send updated value to the GUI
+			break;
+		case 'f':
+			incrementFrequency(freq_fac);
+			Serial.println(F("AudioSynthWaveformSine_F32_UI: changed frequency to ") + String(getFrequency_Hz(),1) + " Hz");
+			updateCard_freq();  //send updated value to the GUI
+			break;
+		case 'F':
+			incrementFrequency(1.0/freq_fac);
+			Serial.println(F("AudioSynthWaveformSine_F32_UI: changed frequency to ") + String(getFrequency_Hz(),1) + " Hz");
+			updateCard_freq();  //send updated value to the GUI
+			break;
+		default:
+			return_val = false;  //we did not process this character
+	}
+
+	return return_val;	
+}
+
+
+//Make a bunch of cards (button groups) to adjust the different algorithm parameters.
+//These are all simple cards that allow you to adjust the parameter up and down based on a character command.
+//Note that the addCardPreset_UpDown is in SerialManager_UI.h...and it automatically prepends the prefix() to the cmd chars
+TR_Card* AudioSynthWaveformSine_F32_UI::addCard_freq( TR_Page *page_h)      { return addCardPreset_UpDown(page_h, "Frequency (Hz)",    "freq",     "F", "f");};
+TR_Card* AudioSynthWaveformSine_F32_UI::addCard_amp_dBFS(  TR_Page *page_h) { return addCardPreset_UpDown(page_h, "Amplitude (dBFS)",  "amp_dB",   "A", "a");};
+
+//add an on/off card for the enable
+TR_Card* AudioSynthWaveformSine_F32_UI::addCard_enable(TR_Page *page_h) { 
+	if (page_h == NULL) return NULL;
+	TR_Card *card_h = page_h->addCard("Play Tone");
+	if (card_h == NULL) return NULL;
+
+	String prefix = getPrefix();   //getPrefix() is in SerialManager_UI.h, unless it is over-ridden in this class somewhere
+	String field_name1 = ID_char_fn + String("on");
+	String field_name2 = ID_char_fn + String("off");
+	card_h->addButton("On",   prefix+"e",  field_name1, 6);  //label, command, id, width
+	card_h->addButton("Off",  prefix+"E",  field_name2, 6);  //label, command, id, width
+	
+	return card_h;
+}
+
+
+//Add a default for single-page control of this sinewave generator
+TR_Page* AudioSynthWaveformSine_F32_UI::addPage_default(TympanRemoteFormatter *gui) {
+	if (gui == NULL) return NULL;
+	TR_Page *page_h = gui->addPage(name_for_UI);
+	if (page_h == NULL) return NULL;
+	
+	//add the default cards
+	addCard_enable(page_h);
+	addCard_freq(page_h);
+	addCard_amp_dBFS(page_h);
+
+	return page_h;	
+}		
+		
+//Make a bunch of methods to update the value of the parameter value shown in the app.
+//Because these were all created using "addCardPreset_UpDown()", let's use the related
+//method  "updateCardPreset_UpDown()", which also lives in SerialManager_UI.h
+void AudioSynthWaveformSine_F32_UI::updateCard_freq(void) {
+	float value = getFrequency_Hz();
+	String val_str = String(value,1);
+	if (value >= 9999.999999f) val_str = String(value,0);
+	updateCardPreset_UpDown("freq", val_str); 
+}
+void AudioSynthWaveformSine_F32_UI::updateCard_amp_dBFS(void){ updateCardPreset_UpDown("amp_dB",   String(getAmplitude_dBFS(),1)); }
+
+//update the on/off field
+void AudioSynthWaveformSine_F32_UI::updateCard_enable(void) { 
+	bool enabled = getEnabled();
+	String field_name1 = ID_char_fn + String("on");
+	String field_name2 = ID_char_fn + String("off");
+	setButtonState(field_name1,enabled);
+	setButtonState(field_name2,!enabled);
+}
+
+//Update all the fields
+ void AudioSynthWaveformSine_F32_UI::setFullGUIState(bool activeButtonsOnly) {
+  updateCard_enable();
+	updateCard_freq();
+	updateCard_amp_dBFS(); 
+ }		
+		
 
 
 
--- src/synth_sine_F32.h
+++ src/synth_sine_F32.h
@@ -16,6 +16,38 @@
 #include <Arduino.h>
 #include "AudioStream_F32.h"
 #include "arm_math.h"
+#include <SerialManager_UI.h>
+
+class AudioSynthWaveformSine_F32; //forward declare.  to be fully defined later in this file
+
+//This class helps manage some of the configuration and state information of the AudioSynthWaveformSine_F32
+//class.  By having this class, it tries to put everything in one place.  It is also helpful for
+//managing the GUI on the TympanRemote mobile App.
+class AudioSynthWaveformSineState {
+	public:
+		AudioSynthWaveformSineState(void) {}; //nothing to do in the constructor;
+	
+		//The sinewave is tricky because it has several configuration parameters that I'd like to track here in
+		//this State-tracking class because I want to access them via the App's GUI.  But, if I keep a local copy
+		//of the parameter values, I need to do a lot of management to ensure that they are in-sync with the underlying
+		//AudioSynthWaveformSine_F32 class.  So, instead, I'm going to just get the values from the class itself.
+	
+		//get parameter values from the compressors
+		bool getEnabled(void);
+		bool setEnabled(bool);
+		float getSampleRate_Hz(void);
+		float getFrequency_Hz(void);
+		float getAmplitude(void);
+		float getAmplitude_dBFS(void);
+
+		//These methods are not used to directly maintain the state of the AudioEffectCompWDRC.
+		//They are supporting methods.
+		void setSynthWaveform(AudioSynthWaveformSine_F32 *c);
+		void printWaveformParameters(void);
+  
+	protected:
+		AudioSynthWaveformSine_F32 *synthWaveform;  //will be an array of pointers to our compressors	
+};
 
 
 class AudioSynthWaveformSine_F32 : public AudioStream_F32
@@ -23,17 +55,20 @@ class AudioSynthWaveformSine_F32 : public AudioStream_F32
 //GUI: inputs:0, outputs:1 //this line used for automatic generation of GUI node
 //GUI: shortName:sine  //this line used for automatic generation of GUI node
 public:
-	AudioSynthWaveformSine_F32() : AudioStream_F32(0, NULL), magnitude(16384) { } //uses default AUDIO_SAMPLE_RATE from AudioStream.h
-	AudioSynthWaveformSine_F32(const AudioSettings_F32 &settings) : AudioStream_F32(0, NULL), magnitude(16384) {
+	AudioSynthWaveformSine_F32() : AudioStream_F32(0, NULL) { 
+		setDefaultValues();
+	} //uses default AUDIO_SAMPLE_RATE from AudioStream.h
+	AudioSynthWaveformSine_F32(const AudioSettings_F32 &settings) : AudioStream_F32(0, NULL) {
+		setDefaultValues();
 		setSampleRate_Hz(settings.sample_rate_Hz);
 	}
-	void frequency(float freq) {
+	virtual void frequency(float freq) {
 		if (freq < 0.0) freq = 0.0;
 		else if (freq > sample_rate_Hz/2.f) freq = sample_rate_Hz/2.f;
 		frequency_Hz = freq;
 		phase_increment = freq * (4294967296.0 / sample_rate_Hz);
 	}
-	void phase(float angle) {
+	virtual void phase(float angle) {
 		if (angle < 0.0f) angle = 0.0f;
 		else if (angle > 360.0f) {
 			angle = angle - 360.0f;
@@ -41,33 +76,93 @@ public:
 		}
 		phase_accumulator = angle * (4294967296.0f / 360.0f);
 	}
-	void amplitude(float n) {
+	virtual void amplitude(float n) {
 		if (n < 0) n = 0;
 		else if (n > 1.0f) n = 1.0f;
 		magnitude = n * magnitude_scale_factor;
 	}
-	void setSampleRate_Hz(const float &fs_Hz) {
+	virtual float getSampleRate_Hz(void) { return sample_rate_Hz; }
+	virtual void setSampleRate_Hz(const float &fs_Hz) {
 		phase_increment *= sample_rate_Hz / fs_Hz; //change the phase increment for the new frequency
 		sample_rate_Hz = fs_Hz;
 	}
-	void begin(void) { enabled = true; }
-	void end(void) { enabled = false; }
+	virtual void begin(void) { setEnabled(true); }
+	virtual void end(void) { setEnabled(false); }
 	virtual void update(void);
-	float getFrequency_Hz(void) { return frequency_Hz; }
-	float setFrequency_Hz(float _freq_Hz) { frequency(_freq_Hz); return getFrequency_Hz(); }
-	float getAmplitude(void) { return (magnitude/magnitude_scale_factor); }
-	float setAmplitude(float amp) { amplitude(amp); return getAmplitude(); }
+	virtual float getFrequency_Hz(void) { return frequency_Hz; }
+	virtual float setFrequency_Hz(float _freq_Hz) { frequency(_freq_Hz); return getFrequency_Hz(); }
+	virtual float getAmplitude(void) { return (magnitude/magnitude_scale_factor); }
+	virtual float setAmplitude(float amp) { amplitude(amp); return getAmplitude(); }
+	virtual float getAmplitude_dBFS(void) { return 10.0*log10f(pow(getAmplitude() / sqrtf(2.0), 2.0)); } //covert peak to RMS
+	virtual float setAmplitude_dBFS(float amp_dBFS) { setAmplitude(sqrtf(pow(10.0,amp_dBFS/10.0))*sqrtf(2.0)); return getAmplitude_dBFS(); } //dBFS is an RMS-type value, so the sqrtf(2.0) converts it to a peak-type value
 	
-private:
+	virtual float incrementAmplitude_dBFS(float incr_dB) { return setAmplitude_dBFS(getAmplitude_dBFS()+incr_dB); }
+	virtual float incrementFrequency(float incr_fac) { return setFrequency_Hz(getFrequency_Hz()*incr_fac); }
+	virtual float incrementFrequency_Hz(float incr_Hz) { return setFrequency_Hz(getFrequency_Hz()+incr_Hz); }
+	
+	virtual bool setEnabled(bool _enable) { return enabled = _enable; }
+	virtual bool getEnabled(void) { return enabled; }
+	
+	AudioSynthWaveformSineState state;
+	
+protected:
 	uint32_t phase_accumulator = 0;
 	uint32_t phase_increment = 0;
 	int32_t magnitude = 0;
 	float sample_rate_Hz = AUDIO_SAMPLE_RATE;
-	volatile uint8_t enabled = 1;
+	volatile bool enabled = true;
 	float32_t frequency_Hz;
 	const float magnitude_scale_factor = 65536.0f;
 	unsigned int block_counter=0;
+	virtual void setDefaultValues(void);
 };
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// UI Versions of the Class
+//
+// This "UI" version of the class adds no signal processing functionality.  Instead, it adds to the
+// class simply to make it easier to add a menu-based or App-based interface to configure and to 
+// control the audio-processing in AudioEffectCompWDRC_F32 above.
+//
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+
+class AudioSynthWaveformSine_F32_UI: public AudioSynthWaveformSine_F32, public SerialManager_UI {
+	public:
+		AudioSynthWaveformSine_F32_UI(void) : 	AudioSynthWaveformSine_F32(), SerialManager_UI() {};
+		AudioSynthWaveformSine_F32_UI(const AudioSettings_F32 settings): AudioSynthWaveformSine_F32(settings), SerialManager_UI() {		};
+		
+		// ///////// here are the methods that you must implement from SerialManager_UI
+		virtual void printHelp(void);
+		//virtual bool processCharacter(char c); //not used here
+		virtual bool processCharacterTriple(char mode_char, char chan_char, char data_char);
+		virtual void setFullGUIState(bool activeButtonsOnly = false); 
+		// ///////// end of required methods
+	
+		//create the button sets for the TympanRemote's GUI
+		TR_Card* addCard_enable(TR_Page* page_h);
+		TR_Card* addCard_freq(TR_Page *page_h);
+		//TR_Card* addCard_amp( TR_Page *page_h);
+		TR_Card* addCard_amp_dBFS( TR_Page *page_h);
+		TR_Page* addPage_default(TympanRemoteFormatter *gui);
+		
+		//methods to update the GUI fields
+		void updateCard_enable(void);
+		void updateCard_freq(void);
+		//void updateCard_amp(void); 
+		void updateCard_amp_dBFS(void); 
+			
+		//here are the factors to use to increment different parameters
+		float amp_fac_dB = 3.0;
+		float freq_fac = powf(2.0,1.0/3.0); //third octave steps
+		
+		String name_for_UI = "Sine Wave";  //used for printHelp()
+	
+	protected:	
+		
+};
+
 
 
 
