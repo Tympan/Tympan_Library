2338792f409a011c5b228581211521ac77d1915a
Chip Audette <chipaudette@yahoo.com>
2024-09-24 11:54:56 -0400

    AudioMixer, AudioSwitch: improve whitespace

--- src/AudioMixer_F32.h
+++ src/AudioMixer_F32.h
@@ -21,32 +21,33 @@
 #include "AudioStream_F32.h"
 
 class AudioMixer4_F32 : public AudioStream_F32 {
-//GUI: inputs:4, outputs:1  //this line used for automatic generation of GUI node
-//GUI: shortName:Mixer4
-public:
+	//GUI: inputs:4, outputs:1  //this line used for automatic generation of GUI node
+	//GUI: shortName:Mixer4
+	public:
     AudioMixer4_F32() : AudioStream_F32(4, inputQueueArray) { setDefaultValues(); }
-	AudioMixer4_F32(const AudioSettings_F32 &settings) : AudioStream_F32(4, inputQueueArray) { setDefaultValues(); }
+		AudioMixer4_F32(const AudioSettings_F32 &settings) : AudioStream_F32(4, inputQueueArray) { setDefaultValues(); }
 	
-	void setDefaultValues(void) {
-		for (int i=0; i<4; i++) multiplier[i] = 1.0;
-	}
-	
-    virtual void update(void);
-	virtual int processData(audio_block_f32_t *audio_in[4], audio_block_f32_t *audio_out); //audio_in can be read-only as no calculations are in-place
-	
-    void gain(unsigned int channel, float gain) {
-      if ((channel >= 4) || (channel < 0)) return;
-      multiplier[channel] = gain;
-    }
-	void mute(void) { for (int i=0; i < 4; i++) gain(i,0.0); };  //mute all channels
-	
-	int switchChannel(unsigned int channel) { 
-		//mute all channels except the given one.  Set the given one to 1.0.
-		if ((channel >= 4) || (channel < 0)) return -1;
-		mute(); 
-		gain(channel,1.0);
-		return channel;
-	} 
+		void setDefaultValues(void) {
+			for (int i=0; i<4; i++) multiplier[i] = 1.0;
+		}
+		
+		virtual void update(void);
+		
+		virtual int processData(audio_block_f32_t *audio_in[4], audio_block_f32_t *audio_out); //audio_in can be read-only as no calculations are in-place
+		
+		void gain(unsigned int channel, float gain) {
+			if ((channel >= 4) || (channel < 0)) return;
+			multiplier[channel] = gain;
+		}
+		void mute(void) { for (int i=0; i < 4; i++) gain(i,0.0); };  //mute all channels
+		
+		int switchChannel(unsigned int channel) { 
+			//mute all channels except the given one.  Set the given one to 1.0.
+			if ((channel >= 4) || (channel < 0)) return -1;
+			mute(); 
+			gain(channel,1.0);
+			return channel;
+		} 
 
   private:
     audio_block_f32_t *inputQueueArray[4];
@@ -54,66 +55,70 @@ public:
 };
 
 class AudioMixer8_F32 : public AudioStream_F32 {
-//GUI: inputs:8, outputs:1  //this line used for automatic generation of GUI node
-//GUI: shortName:Mixer8
-public:
-    AudioMixer8_F32() : AudioStream_F32(8, inputQueueArray) { setDefaultValues();}
-    AudioMixer8_F32(const AudioSettings_F32 &settings) : AudioStream_F32(8, inputQueueArray) { setDefaultValues();}
+	//GUI: inputs:8, outputs:1  //this line used for automatic generation of GUI node
+	//GUI: shortName:Mixer8
+	public:
+		AudioMixer8_F32() : AudioStream_F32(8, inputQueueArray) { setDefaultValues();}
+		AudioMixer8_F32(const AudioSettings_F32 &settings) : AudioStream_F32(8, inputQueueArray) { setDefaultValues();}
+		
+		void setDefaultValues(void) {
+				for (int i=0; i<8; i++) multiplier[i] = 1.0;
+			}
+
+		virtual void update(void);
+
+		void gain(unsigned int channel, float gain) {
+			if ((channel >= 8) || (channel < 0)) return;
+			multiplier[channel] = gain;
+		}
 	
-	void setDefaultValues(void) {
-      for (int i=0; i<8; i++) multiplier[i] = 1.0;
-    }
+		void mute(void) { for (int i=0; i < 8; i++) gain(i,0.0); };  //mute all channels
+		
+		int switchChannel(unsigned int channel) { 
+			//mute all channels except the given one.  Set the given one to 1.0.
+			if ((channel >= 8) || (channel < 0)) return -1;
+			mute(); 
+			gain(channel,1.0);
+			return channel;
+		} 
 
-    virtual void update(void);
-
-    void gain(unsigned int channel, float gain) {
-      if ((channel >= 8) || (channel < 0)) return;
-      multiplier[channel] = gain;
-    }
-	void mute(void) { for (int i=0; i < 8; i++) gain(i,0.0); };  //mute all channels
-	int switchChannel(unsigned int channel) { 
-		//mute all channels except the given one.  Set the given one to 1.0.
-		if ((channel >= 8) || (channel < 0)) return -1;
-		mute(); 
-		gain(channel,1.0);
-		return channel;
-	} 
-
-  private:
-    audio_block_f32_t *inputQueueArray[8];
-    float multiplier[8];
+	private:
+		audio_block_f32_t *inputQueueArray[8];
+		float multiplier[8];
 };
 
 #define MIXER_N_CHAN_MAX 16
 class AudioMixer16_F32 : public AudioStream_F32 {
-//GUI: inputs:16, outputs:1  //this line used for automatic generation of GUI node
-//GUI: shortName:Mixer16
-public:
-    AudioMixer16_F32() : AudioStream_F32(MIXER_N_CHAN_MAX, inputQueueArray) { setDefaultValues();}
-    AudioMixer16_F32(const AudioSettings_F32 &settings) : AudioStream_F32(MIXER_N_CHAN_MAX, inputQueueArray) { setDefaultValues();}
-	
-	void setDefaultValues(void) {
-      for (int i=0; i<n_chan; i++) multiplier[i] = 1.0;
-    }
+	//GUI: inputs:16, outputs:1  //this line used for automatic generation of GUI node
+	//GUI: shortName:Mixer16
+	public:
+			AudioMixer16_F32() : AudioStream_F32(MIXER_N_CHAN_MAX, inputQueueArray) { setDefaultValues();}
+			AudioMixer16_F32(const AudioSettings_F32 &settings) : AudioStream_F32(MIXER_N_CHAN_MAX, inputQueueArray) { setDefaultValues();}
+		
+		void setDefaultValues(void) {
+				for (int i=0; i<n_chan; i++) multiplier[i] = 1.0;
+			}
 
-    virtual void update(void);
+		virtual void update(void);
 
-    void gain(unsigned int channel, float gain) {
-      if ((channel >= (unsigned int)n_chan) || (channel < 0)) return;
-      multiplier[channel] = gain;
-    }
-	void mute(void) { for (int i=0; i < n_chan; i++) gain(i,0.0); };  //mute all channels
-	int switchChannel(unsigned int channel) { 
-		//mute all channels except the given one.  Set the given one to 1.0.
-		if ((channel >= (unsigned int)n_chan) || (channel < 0)) return -1;
-		mute(); 
-		gain(channel,1.0);
-		return channel;
-	} 
+		void gain(unsigned int channel, float gain) {
+			if ((channel >= (unsigned int)n_chan) || (channel < 0)) return;
+			multiplier[channel] = gain;
+		}
+		
+		void mute(void) { for (int i=0; i < n_chan; i++) gain(i,0.0); };  //mute all channels
+		
+		int switchChannel(unsigned int channel) { 
+			//mute all channels except the given one.  Set the given one to 1.0.
+			if ((channel >= (unsigned int)n_chan) || (channel < 0)) return -1;
+			mute(); 
+			gain(channel,1.0);
+			return channel;
+		} 
 
   private:
-	const int n_chan = MIXER_N_CHAN_MAX;
-    audio_block_f32_t *inputQueueArray[MIXER_N_CHAN_MAX];
+		const int n_chan = MIXER_N_CHAN_MAX;
+		audio_block_f32_t *inputQueueArray[MIXER_N_CHAN_MAX];
     float multiplier[MIXER_N_CHAN_MAX];
 };
 
--- src/AudioSwitch_F32.h
+++ src/AudioSwitch_F32.h
@@ -22,19 +22,19 @@ class AudioSwitch4_F32 : public AudioStream_F32 {
 //GUI: inputs:1, outputs:4  //this line used for automatic generation of GUI node
 //GUI: shortName:Switch4
 public:
-    AudioSwitch4_F32() : AudioStream_F32(1, inputQueueArray) { setDefaultValues(); }
+  AudioSwitch4_F32() : AudioStream_F32(1, inputQueueArray) { setDefaultValues(); }
 	AudioSwitch4_F32(const AudioSettings_F32 &settings) : AudioStream_F32(1, inputQueueArray) { setDefaultValues(); }
 	
 	void setDefaultValues(void) {
 		outputChannel = 0;
 	}
 	
-    virtual void update(void);
+	virtual void update(void);
 
-    int setChannel(unsigned int channel) {
-      if (channel >= 4 || channel < 0) return outputChannel;  //invalid!  stick with previous channel
-      return outputChannel = channel;
-    }
+	int setChannel(unsigned int channel) {
+		if (channel >= 4 || channel < 0) return outputChannel;  //invalid!  stick with previous channel
+		return outputChannel = channel;
+	}
 
   private:
     audio_block_f32_t *inputQueueArray[1];
