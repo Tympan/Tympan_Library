1e56fa6d134f4f2102404abfdef9649f5cf35b56
Chip Audette <chipaudette@yahoo.com>
2025-11-11 17:20:55 -0500

    Examples: add nRF52 (Rev F) example for QueuedTX

---
+++ examples/02-Utility/Bluetooth/Tympan_Test_BLE_nRF52_QueuedTX/SerialManager.h
@@ -0,0 +1,402 @@
+
+#ifndef _SerialManager_h
+#define _SerialManager_h
+
+#include <Tympan_Library.h>
+#include "State.h"
+
+//classes from the main sketch that might be used here
+extern Tympan myTympan;                  //created in the main *.ino file
+extern State myState;                    //created in the main *.ino file
+extern AudioSettings_F32 audio_settings; //created in the main *.ino file  
+extern BLE_nRF52 &ble;
+
+
+//functions in the main sketch that I want to call from here
+extern void changeGain(float);
+extern void printGainLevels(void);
+extern bool flag_useBleQueue;
+
+//
+// The purpose of this class is to be a central place to handle all of the interactions
+// to and from the SerialMonitor or TympanRemote App.  Therefore, this class does things like:
+//
+//    * Define what buttons and whatnot are in the TympanRemote App GUI
+//    * Define what commands that your system will respond to, whether from the SerialMonitor or from the GUI
+//    * Send updates to the GUI based on the changing state of the system
+//
+// You could achieve all of these goals without creating a dedicated class.  But, it 
+// is good practice to try to encapsulate some of these functions so that, when the
+// rest of your code calls functions related to the serial communciation (USB or App),
+// you have a better idea of where to look for the code and what other code it relates to.
+//
+
+String str_queueNotSupported = "queueMessage() not supported by your BLE module firmware.  Using sendMessage().";
+
+void setButtonState(const String &btnId, bool newState) {
+  String msg = String("STATE=BTN:" + btnId + ":1");
+  //Serial.println("serialManager: setButtonState: sending = " + msg); //echo to USB Serial for debugging
+  if (flag_useBleQueue) {
+    ble.queueMessage(msg);
+  } else {
+    Serial.println("SerialManager: setButtonState: " + str_queueNotSupported);
+    ble.sendMessage(msg);
+  }
+}
+void setButtonText(const String &btnId, String text) {
+  String msg = String("TEXT=BTN:" + btnId + ":" + text);
+  //Serial.println("serialManager: setButtonText: sending = " + msg); //echo to USB Serial for debugging
+  if (flag_useBleQueue) {
+    ble.queueMessage(msg);
+  } else {
+    Serial.println("SerialManager: setButtonText:  " + str_queueNotSupported);
+    ble.sendMessage(msg);
+  }
+}
+
+void sendLotsOfData(int approx_num_bytes) {
+  int total_sent = 0;
+  uint8_t packet_counter = 0;
+  String body = "-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  while (total_sent < approx_num_bytes) {
+    //compose the message string
+    String msg = "_";  //lead off with this character
+    if (packet_counter < 100) msg += "0"; //add leading zero, if needed
+    if (packet_counter < 10)msg += "0";   //add another leading zero, if needed
+    msg += String(packet_counter);        //add the packet counter just for fun
+    msg += body;                          //add the bulk of the message
+
+    //send the message string
+    if (flag_useBleQueue) {
+      ble.queueMessage(msg);
+    } else {
+      Serial.println("SerialManager: sendLotsOfData: queueMessage() not supported by your BLE module firmware.  Using sendMessage().");
+      ble.sendMessage(msg);
+    }
+    total_sent += msg.length();
+  }
+}
+
+
+//now, define the Serial Manager class
+class SerialManager  {  // see Tympan_Library for SerialManagerBase for more functions!
+  public:
+    SerialManager(void)  {};
+      
+    void printHelp(void);
+    void createTympanRemoteLayout(void); 
+    void printTympanRemoteLayout(void); 
+    bool respondToByte(char c);
+    bool processCharacter(char c);  //this is called automatically by SerialManagerBase.respondToByte(char c)
+
+    //method for updating the GUI on the App
+    void setFullGUIState(bool activeButtonsOnly = false);
+    void updateGainDisplay(void);
+    void updateCpuDisplayOnOff(void);
+    void updateCpuDisplayUsage(void);
+
+    //factors by which to raise or lower the parameters when receiving commands from TympanRemote App
+    float gainIncrement_dB = 3.0f;            //raise or lower by x dB
+  private:
+    TympanRemoteFormatter myGUI;  //Creates the GUI-writing class for interacting with TympanRemote App
+   
+};
+
+void SerialManager::printHelp(void) {  
+  Serial.println("SerialManager Help: Available Commands:");
+  Serial.println("    h: Print this help");
+  Serial.println("    k/K: AUDIO: Incr/Decrease Digital Gain");
+  Serial.println("    c/C: SYSTEM: Enable/Disable printing of CPU and Memory usage");
+  Serial.println("    v:   BLE: Get 'Version' of firmware from module");
+  Serial.println("    n:   BLE: Get 'Name' from module");
+  Serial.println("    N:   BLE: Set 'Name' of module to 'TYMP-TYMP'");
+  Serial.println("    t:   BLE: Get 'Advertising' status via software");
+  Serial.println("    f/F: BLE: Set 'Advertising' to ON (f) or OFF (F)");
+  Serial.println("    g/G: BLE: Get 'Connected' status via software (g) or GPIO (G)");
+  Serial.println("    m:   BLE: Get 'LED Mode' setting (0=OFF, 1=AUTO)");
+  Serial.println("    b/B: BLE: Set 'LED Mode' to AUTO (b) or OFF (B)");
+  Serial.println("    J:   Send JSON for the GUI for the Tympan Remote App");
+  Serial.println("  : Special commands related to the BLE module's TX queue:");
+  Serial.println("    d/D: BLE QUEUE: Set delay to (d) 10 msec or (D) 99 msec");
+  Serial.println("    q  : BLE QUEUE: Get the current TX delay between messages");
+  Serial.println("    w  : BLE QUEUE: Get the messages currently in the queue");
+  Serial.println("    W  : BLE QUEUE: Get the free bytes in the queue");
+  Serial.println("    e  : BLE QUEUE: Clear the queue (stop them from being transmitted.");
+  Serial.println("    a/A : Send many messages to be queued: (a) 1 kB, (A) 50kB");
+  Serial.println();
+}
+
+bool SerialManager::respondToByte(char c) {
+  //Serial.println("SerialManager: respondToByte " + String(c));
+  return processCharacter(c);
+}
+
+//switch yard to determine the desired action
+bool SerialManager::processCharacter(char c) {  //this is called by SerialManagerBase.respondToByte(char c)
+  bool ret_val = true;
+  int val = 0;
+  int err_code = 0;
+
+  switch (c) {
+    case 'h':
+      printHelp(); 
+      break;
+   case 'k':
+      changeGain(gainIncrement_dB);   //raise
+      printGainLevels();      //print to USB Serial (ie, to the SerialMonitor)
+      updateGainDisplay();    //update the App
+      break;
+    case 'K':
+      changeGain(-gainIncrement_dB);  //lower
+      printGainLevels();     //print to USB Serial (ie, to the SerialMonitor)
+      updateGainDisplay();   //update the App
+      break;
+    case 'c':
+      Serial.println("Starting CPU reporting...");
+      myState.printCPUtoGUI = true;
+      updateCpuDisplayOnOff();
+      break;
+    case 'C':
+      Serial.println("Stopping CPU reporting...");
+      myState.printCPUtoGUI = false;
+      updateCpuDisplayOnOff();
+      break;  
+    case 'n':
+      {
+        String name = String("");
+        int err_code = ble.getBleName(&name);
+        Serial.println("serialManager: BLE: Name from module = " + name);
+        if (err_code != 0) Serial.println("serialManager:  ble.getBleName returned error code " + String(err_code));
+        setButtonText("bleName",name);
+      }
+      break;
+    case 'N':
+      {
+        String name = String("TympTymp");
+        Serial.println("serialManager: BLE: Set Name of module to " + name);
+        int err_code = ble.setBleName(name);
+        if (err_code != 0) Serial.println("serialManager:  ble.setBleName returned error code " + String(err_code));
+      }
+      break;
+    case 't':
+      val = ble.isAdvertising();
+      Serial.println("serialManager: BLE: Advertising = " + String(val));
+      setButtonText("isAdvert", String(val ? "ON" : "OFF"));
+      break;
+    case 'f':
+      Serial.println("serialManager: BLE: Set Advertising to ON...");
+      err_code = ble.enableAdvertising(true);
+      if (err_code) Serial.println("SerialManager: ble.enableAdvertising returned err_code = " + String(err_code));
+      break;
+    case 'F':
+      Serial.println("serialManager: BLE: Set Advertising to OFF...");
+      err_code = ble.enableAdvertising(false);
+      if (err_code) Serial.println("SerialManager: ble.enableAdvertising returned err_code = " + String(err_code));
+      break;
+    case 'm':
+      val = ble.getLedMode();
+      if (val < 0) {
+        Serial.println("SerialManager: ble.getLedMode returned err_code = " + String(val));
+      } else {
+        Serial.println("serialManager: BLE: LED Mode = " + String(val) + " (1=AUTO, 0=OFF)");
+        setButtonText("ledMode", String(val ? "AUTO" : "OFF"));
+      }
+      break;
+    case 'b':
+      Serial.println("serialManager: BLE: Set LED Mode to 1 (AUTO)");
+      err_code = ble.setLedMode(1);
+      if (err_code < 0) {
+        Serial.println("SerialManager: ble.setLedMode returned err_code = " + String(err_code));
+      } else {
+        delay(5); 
+        Serial.println("serialManager: following up by issuing ble.getLedMode()...");
+        val = ble.getLedMode();  
+        if (val < 0) {
+          Serial.println("serialManager: ble.getLedMode returned err_code = " + String(val));  
+        } else {
+          Serial.println("serialManager: BLE: LED Mode = " + String(val) + " (1=AUTO, 0=OFF)");
+          setButtonText("ledMode", String(val ? "AUTO" : "OFF"));
+        }
+      }
+      break;
+    case 'B':
+      Serial.println("serialManager: BLE: Set LED Mode to 0 (OFF)");
+      err_code = ble.setLedMode(0);
+      if (err_code < 0) {
+        Serial.println("SerialManager: ble.setLedMode returned err_code = " + String(err_code));
+      } else {
+        delay(5); 
+        Serial.println("serialManager: following up by issuing ble.getLedMode()...");
+        val = ble.getLedMode();  
+        if (val < 0) {
+          Serial.println("SerialManager: ble.getLedMode returned err_code = " + String(val));  
+        } else {
+          Serial.println("serialManager: BLE: LED Mode = " + String(val) + " (1=AUTO, 0=OFF)");
+          setButtonText("ledMode", String(val ? "AUTO" : "OFF"));
+        }
+      }  
+      break; 
+    case 'g':
+      val = ble.isConnected(BLE_nRF52::GET_VIA_SOFTWARE);
+      if (val < 0) {
+        Serial.println("SerialManager: ble.isConnected(Software) returned err_code = " + String(val));
+      } else {
+        Serial.println("serialManager: BLE: Connected (via software) = " + String(val));
+        setButtonText("isConn_s", String(val ? "YES" : "NO"));
+        setButtonText("isConn_g", String(""));
+      }
+      break;
+    case 'G':
+      val = ble.isConnected(BLE_nRF52::GET_VIA_GPIO);
+      if (val < 0) {
+        Serial.println("SerialManager: ble.isConnected(GPIO) returned err_code = " + String(val));
+      } else {
+        Serial.println("serialManager: BLE: Connected (via GPIO) = " + String(val));
+        setButtonText("isConn_g", String(val ? "YES" : "NO"));
+        setButtonText("isConn_s", String(""));
+      }
+      break;
+    case 'v':
+      {
+        String version;
+        err_code = ble.version(&version);
+        if (err_code < 0) {
+          Serial.println("serialManager: BLE: version returned err_code " + String(err_code));
+        } else {
+          Serial.println("serialManager: BLE module firmware: " + version);
+          setButtonText("version",version);
+        }
+      }
+      break; 
+    case 'd':
+      val = ble.setTxQueueDelay_msec(10);
+      Serial.println("serialManager: BLE Module's Queue: set delay to 10 msec (Reply = " +String(val) + ")");
+      break;
+    case 'D':
+      val = ble.setTxQueueDelay_msec(99);
+      Serial.println("serialManager: BLE Module's Queue: set delay to 99 msec (Reply = " +String(val) + ")");
+      break;
+    case 'q':
+      val = ble.getTxQueueDelay_msec();
+      Serial.println("serialManager: BLE Module's Queue: module says delay is " +String(val) + " msec (zero or negative are errors)");
+      break;
+    case 'w':
+      val = ble.getTxQueueNumMsgs();
+      Serial.println("serialManager: BLE Module's Queue: " +String(val) + " messages in queue");
+      break;
+    case 'W':
+      val = ble.getTxQueueFreeBytes();
+      Serial.println("serialManager: BLE Module's Queue: " +String(val) + " free bytes in queue");
+      break;
+    case 'e':
+      val = ble.clearTxQueue();
+      Serial.println("serialManager: BLE Module's Queue: cleared TX queue (Reply = " +String(val) + ")");
+      break;  
+    case 'a':
+      Serial.println("serialManager: Sending about 1 kB of packets to be queued by the module");
+      sendLotsOfData(1000);
+      break;
+    case 'A':
+      Serial.println("serialManager: Sending about 50 kB of packets to be queued by the module");
+      sendLotsOfData(50000);
+      break;
+
+    case 'J': case 'j':           //The TympanRemote app sends a 'J' to the Tympan when it connects
+      printTympanRemoteLayout();  //in resonse, the Tympan sends the definition of the GUI that we'd like
+      break;
+    default:
+      ret_val = false;
+  }
+  return ret_val;
+}
+
+
+// //////////////////////////////////  Methods for defining and transmitting the GUI to the App
+
+void SerialManager::createTympanRemoteLayout(void) {
+  
+  // Create some temporary variables
+  TR_Page *page_h;  //dummy handle for a page
+  TR_Card *card_h;  //dummy handle for a card
+
+  //Add first page to GUI  (the indentation doesn't matter; it is only to help us see it better)
+  page_h = myGUI.addPage("nRF52 BLE Testing");
+      //Add a card to this page
+      card_h = page_h->addCard("BLE Name");
+          card_h->addButton("Get Name","n",""     , 12);
+          card_h->addButton("", "", "bleName", 12);  //displayed string (blank for now), command (blank), button ID, button width (out of 12)
+
+      //Add a card to this page
+      card_h = page_h->addCard("BLE Firmware");
+          card_h->addButton("Get Version","v","", 12);
+          card_h->addButton("",  "", "version",   12);  //displayed string (blank for now), command (blank), button ID, button width (out of 12)
+
+      //Add a card to this page
+      card_h = page_h->addCard("Bluetooth LED");
+          card_h->addButton("Get Mode","m",""     , 6);
+          card_h->addButton("", "", "ledMode", 6);  //displayed string (blank for now), command (blank), button ID, button width (out of 12)
+          card_h->addButton("AUTO On","b","",     6);  //displayed string, command, button ID, button width (out of 12)
+          card_h->addButton("AUTO Off","B","",      6);  //displayed string, command, button ID, button width (out of 12)
+          
+  //Add a second page to the GUI
+  page_h = myGUI.addPage("Audio");
+
+    //Add an example card that just displays a value...no interactive elements
+    card_h = page_h->addCard(String("Analog Input Gain (dB)"));
+      card_h->addButton("",      "", "inpGain",   12); //label, command, id, width (out of 12)...THIS ISFULL WIDTH!
+
+    card_h = page_h->addCard("Digital Gain (dB)");
+        card_h->addButton("-","K","",           4);  //displayed string, command, button ID, button width (out of 12)
+        card_h->addButton("","","gainIndicator",4);  //displayed string (blank for now), command (blank), button ID, button width (out of 12)
+        card_h->addButton("+","k","",           4);   //displayed string, command, button ID, button width (out of 12)
+
+
+    //Add an example card where one of the buttons will indicate "on" or "off"
+//    card_h = page_h->addCard(String("CPU Usage (%)"));
+//      card_h->addButton("Start", "c", "cpuStart", 4);  //label, command, id, width...we'll light this one up if we're showing CPU usage
+//      card_h->addButton(""     , "",  "cpuValue", 4);  //label, command, id, width...this one will display the actual CPU value
+//      card_h->addButton("Stop",  "C", "",         4);  //label, command, id, width...this one will turn off the CPU display
+        
+  //add some pre-defined pages to the GUI (pages that are built-into the App)
+  myGUI.addPredefinedPage("serialMonitor");
+  //myGUI.addPredefinedPage("serialPlotter");
+}
+
+
+// Print the layout for the Tympan Remote app, in a JSON-ish string
+void SerialManager::printTympanRemoteLayout(void) {
+    Serial.println("SerialManager: printTympanRemoteLayout: sending JSON...");
+    if (myGUI.get_nPages() < 1) createTympanRemoteLayout();  //create the GUI, if it hasn't already been created
+    String s = myGUI.asString();
+    Serial.println(s);
+    if (flag_useBleQueue) {
+      ble.queueMessage(s); //ble is held by SerialManagerBase
+    } else { 
+      Serial.println("SerialManager::printTympanRemoteLayout: "  + str_queueNotSupported);
+      ble.sendMessage(s);
+    }
+    setFullGUIState();
+}
+
+// //////////////////////////////////  Methods for updating the display on the GUI
+
+void SerialManager::setFullGUIState(bool activeButtonsOnly) {
+  updateGainDisplay();
+  setButtonText("inpGain",String(myState.input_gain_dB,1));
+  updateCpuDisplayOnOff();
+}
+
+void SerialManager::updateGainDisplay(void) {
+  setButtonText("gainIndicator", String(myState.digital_gain_dB,1));
+}
+
+
+void SerialManager::updateCpuDisplayOnOff(void) {
+  //setButtonState("cpuStart",myState.printCPUtoGUI);  //illuminate the button if we will be sending the CPU value
+}
+
+void SerialManager::updateCpuDisplayUsage(void) {
+   setButtonText("cpuValue",String(audio_settings.processorUsage(),1)); //one decimal places
+}
+
+#endif
---
+++ examples/02-Utility/Bluetooth/Tympan_Test_BLE_nRF52_QueuedTX/Tympan_Test_BLE_nRF52_QueuedTX.ino
@@ -0,0 +1,186 @@
+/*
+*   Tympan_Test_BLE_nRF52_QueuedTX
+*
+*   Created: Chip Audette, OpenAudio, April 2024
+*            Updated May 2024 for updated BLE_nRF52 in Tympan_Library
+*   Purpose: Test the nRF52840 on the Tympan RevF.  Includes App interaction.
+*            Uses the BLE module's queue to enable the Tympan to dump more many
+*            messages onto the module without the Tympan having to wait for the
+*            transmissions to be complete (requires BLE Firmware 0.5.0 or newer). 
+*
+*   TympanRemote App: https://play.google.com/store/apps/details?id=com.creare.tympanRemote
+*   
+*   For Tympan RevF, which requires you to set the Arduino IDE to compile for Teensy 4.1
+*   
+*   See the comments at the top of SerialManager.h and State.h for more description.
+*
+*   MIT License.  use at your own risk.
+*/
+
+//here are the libraries that we need
+#include <Tympan_Library.h>  //include the Tympan Library
+#include "SerialManager.h"
+#include "State.h"
+
+//set the sample rate and block size
+const float sample_rate_Hz = 44100.0f ; //24000 or 44100 (or 44117, or other frequencies in the table in AudioOutputI2S_F32)
+const int audio_block_samples = 128;     //do not make bigger than AUDIO_BLOCK_SAMPLES from AudioStream.h (which is 128)
+AudioSettings_F32 audio_settings(sample_rate_Hz, audio_block_samples);
+
+//create audio library objects for handling the audio
+Tympan                    myTympan(TympanRev::F,audio_settings);          //only TympanRev::F has the nRF52480
+AudioInputI2S_F32         i2s_in(audio_settings);                         //Digital audio in *from* the Teensy Audio Board ADC.
+AudioEffectGain_F32       gain1(audio_settings), gain2(audio_settings);   //Applies digital gain to audio data.  Left and right.
+AudioOutputI2S_F32        i2s_out(audio_settings);                        //Digital audio out *to* the Teensy Audio Board DAC.
+
+//Make all of the audio connections
+AudioConnection_F32       patchCord1(i2s_in, 0, gain1, 0);   //connect the Left input
+AudioConnection_F32       patchCord2(i2s_in, 1, gain2, 0);   //connect the Right input
+AudioConnection_F32       patchCord5(gain1, 0, i2s_out, 0);     //connect the Left gain to the Left output
+AudioConnection_F32       patchCord6(gain2, 0, i2s_out, 1);     //connect the Right gain to the Right output
+
+// Create classes for controlling the system
+#include      "SerialManager.h"
+#include      "State.h"                            
+BLE_nRF52&    ble = myTympan.getBLE_nRF52(); //get BLE object for the nRF52840 (RevF only!)
+SerialManager serialManager;                 //create the serial manager for real-time control (via USB or App)
+State         myState; //keeping one's state is useful for the App's GUI
+
+bool flag_useBleQueue = true;
+
+// function to check the bluetooth version
+bool isBleFirmwareRevisionOK(bool flag_printInfoToSerial) { //returns true if OK
+  bool flag_firmwareVersionOK = false;
+
+  //get the BLE firmware revision from the BLE module
+  String version_string_reply;
+  ble.version(&version_string_reply);
+  if (flag_printInfoToSerial) Serial.println("isBleFirmwareRevisionOK: BLE Firmware Version is: " + version_string_reply);
+
+  //find first index that is a digit
+  unsigned int ind = 0;
+  int str_len =  (int)version_string_reply.length(); //length() returns unsigned int, which is annoying for doing our subtraction operation later
+  while (ind < str_len) {
+    char c = version_string_reply[ind];
+    if ((c >= '0') || (c <= '9')) break;
+    ind++;
+  }
+  if ((str_len - ind) < 5) return flag_firmwareVersionOK;  //must be of format x.x.x, so must be at least 5 digits long
+
+  //check against the requirement for 0.5.0 or higher
+  if ((version_string_reply[ind] > '0') && (version_string_reply[ind] <= '9')) { flag_firmwareVersionOK = true; return flag_firmwareVersionOK; }     //any 1.x.x or higher is OK
+  if ((version_string_reply[ind+2] >= '5') && (version_string_reply[ind+2] <= '9')) { flag_firmwareVersionOK = true; return flag_firmwareVersionOK; }  //any 0.5.x or higher is OK
+  //if (version_string_reply[ind+4] > '0') { flag_firmwareVersionOK = true; return flag_firmwareVersionOK; } //no check here needed
+  
+  //print warning about version being too old
+  if ((flag_firmwareVersionOK==false) && (flag_printInfoToSerial)) {
+    Serial.println("isBleFirmwareRevisionOK: *** WARNING ***: Your Tympan's BLE module needs newer firmware!  At least 0.5.0 is needed!");
+  }
+
+  return flag_firmwareVersionOK;
+}
+
+// define the setup() function, the function that is called once when the device is booting
+void setup() {
+  //begin the serial comms (for debugging)
+  //Serial.begin(115200);  //USB Serial.  This begin() isn't really needed on Teensy. 
+  myTympan.beginBluetoothSerial(); //should use the correct Serial port and the correct baud rate
+  delay(1000);
+  Serial.println("Tympan_Test_BLE_nRF52: Starting setup()...");
+
+  //allocate the dynamic memory for audio processing blocks
+  AudioMemory_F32(10,audio_settings); 
+
+  //Enable the Tympan to start the audio flowing!
+  myTympan.enable(); // activate AIC
+
+  //Choose the desired input
+  myTympan.inputSelect(TYMPAN_INPUT_ON_BOARD_MIC);     // use the on board microphones
+  //myTympan.inputSelect(TYMPAN_INPUT_JACK_AS_MIC);    // use the microphone jack - defaults to mic bias 2.5V
+  //myTympan.inputSelect(TYMPAN_INPUT_JACK_AS_LINEIN); // use the microphone jack - defaults to mic bias OFF
+
+  //Set the desired volume levels
+  myTympan.volume_dB(myState.output_gain_dB);          // headphone amplifier.  -63.6 to +24 dB in 0.5dB steps.
+  myTympan.setInputGain_dB(myState.input_gain_dB);     // set input volume, 0-47.5dB in 0.5dB setps
+
+  //setup BLE
+  myTympan.setupBLE();
+
+  //get BLE version (to make sure it's new enough)
+  bool printInfoToSerial = true;
+  flag_useBleQueue = isBleFirmwareRevisionOK(printInfoToSerial);
+
+  Serial.println("Setup complete.");
+  serialManager.printHelp();
+} //end setup()
+
+
+// define the loop() function, the function that is repeated over and over for the life of the device
+void loop() {
+
+  //look for in-coming serial messages via USB
+  if (Serial.available()) serialManager.respondToByte((char)Serial.read());   //USB Serial
+
+  //respond to in coming serial messages via BLE
+  if (ble.available() > 0) {
+    String msgFromBle; ble.recvBLE(&msgFromBle);    //get BLE messages (removing non-payload messages)
+    //serialManager.respondToByte(msgFromBle[i]); //interpet each character of the message
+    //Serial.print(msgFromBle);
+    for (unsigned int i=0; i < msgFromBle.length(); i++) {
+      if (msgFromBle[i] == 'J') {
+        serialManager.respondToByte(msgFromBle[i]);
+      } else {
+        Serial.print(msgFromBle[i]);
+      }
+    }
+  }
+
+  //service the BLE advertising state...not needed for RevF as it automatically re-starts advertising
+  //ble.updateAdvertising(millis(),5000); //if not connected, ensure it's advertising (this line only needed for Tympan RevD and RevE)
+
+
+  //periodically print the CPU and Memory Usage
+  if (myState.printCPUtoGUI) {
+    myTympan.printCPUandMemory(millis(),3000); //print every 3000 msec
+    serviceUpdateCPUtoGUI(millis(),3000);      //print every 3000 msec
+  }
+
+} //end loop();
+
+
+// ///////////////// Servicing routines
+
+
+//Test to see if enough time has passed to send up updated CPU value to the App
+void serviceUpdateCPUtoGUI(unsigned long curTime_millis, unsigned long updatePeriod_millis) {
+  static unsigned long lastUpdate_millis = 0;
+
+  //has enough time passed to update everything?
+  if (curTime_millis < lastUpdate_millis) lastUpdate_millis = 0; //handle wrap-around of the clock
+  if ((curTime_millis - lastUpdate_millis) > updatePeriod_millis) { //is it time to update the user interface?
+    //send the latest value to the GUI!
+    serialManager.updateCpuDisplayUsage();
+    
+    lastUpdate_millis = curTime_millis;
+  } // end if
+} //end serviceUpdateCPUtoGUI();
+
+
+
+// ///////////////// functions used to respond to the commands
+
+//change the gain from the App
+void changeGain(float change_in_gain_dB) {
+  myState.digital_gain_dB = myState.digital_gain_dB + change_in_gain_dB;
+  gain1.setGain_dB(myState.digital_gain_dB);  //set the gain of the Left-channel gain processor
+  gain2.setGain_dB(myState.digital_gain_dB);  //set the gain of the Right-channel gain processor
+}
+
+//Print gain levels 
+void printGainLevels(void) {
+  Serial.print("Analog Input Gain (dB) = "); 
+  Serial.println(myState.input_gain_dB); //print text to Serial port for debugging
+  Serial.print("Digital Gain (dB) = "); 
+  Serial.println(myState.digital_gain_dB); //print text to Serial port for debugging
+}
+
